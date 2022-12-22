76eea86b58912f50f1aa0c8d95a74d21e7be273e
Fahad Ali <alifahad2210@gmail.com>
2022-12-22 17:48:13 +0530

    lint fixes

--- src/lib/reaction.js
+++ src/lib/reaction.js
@@ -1,15 +1,14 @@
-/* eslint-disable guard-for-in */
 export const serializeReactions = (reactions) => {
   const arr = [];
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const emoji in reactions) {
+  const emojis = Object.keys(reactions);
+  emojis.forEach((emoji) =>
     arr.push({
       name: emoji,
       count: reactions[emoji].usernames.length,
       usernames: reactions[emoji].usernames,
-    });
-  }
+    })
+  );
 
   return arr;
 };
