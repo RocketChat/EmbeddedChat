a760825fb98d6aeefd29b96c53b696602b5fbb6d
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-02 21:03:50 +0530

    style(constant.js): :art: make constant of keyed items

---
+++ src/lib/constant.js
@@ -0,0 +1,4 @@
+export const RC_LOCAL_USER_ID = 'rc_uid';
+export const RC_LOCAL_USER_NAME = 'rc_uname';
+export const RC_LOCAL_USER_AVATAR_URL = 'rc_avatar_url';
+export const RC_LOCAL_USER_TOKEN = 'rc_token';
