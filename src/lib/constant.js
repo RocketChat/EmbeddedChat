cbb1880de8ee3a11bf268d02625d3d37d64d7154
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-04 14:11:43 +0530

    refactor(Auth): :art: add RC_USER_TOKEN_COOKIE='rc_token' RC_USER_ID_COOKIE='rc_uid' in constant.js

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,4 +1,5 @@
-export const RC_LOCAL_USER_ID = 'rc_uid';
 export const RC_LOCAL_USER_NAME = 'rc_uname';
 export const RC_LOCAL_USER_AVATAR_URL = 'rc_avatar_url';
-export const RC_LOCAL_USER_TOKEN = 'rc_token';
+export const RC_LOCAL_USER_ID = 'rc_uid';
+export const RC_USER_TOKEN_COOKIE = 'rc_token';
+export const RC_USER_ID_COOKIE = 'rc_uid';
