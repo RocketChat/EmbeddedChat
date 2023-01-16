2ed1700504b893055cefa3ea81aa757fbfe33e57
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-16 11:31:50 +0530

    feat(auth): :sparkles: add logic for totp and resend

--- src/lib/api.js
+++ src/lib/api.js
@@ -25,22 +25,42 @@ export default class RocketChatInstance {
     Cookies.set(RC_USER_ID_COOKIE, cookies.rc_uid || '');
   }
 
-  async googleSSOLogin(signIn) {
+  async googleSSOLogin(signIn, acsCode) {
     const tokens = await signIn();
+    let acsPayload = null;
+
+    if (typeof acsCode === 'string') {
+      acsPayload = acsCode;
+    }
+
+    const payload = acsCode
+      ? JSON.stringify({
+          serviceName: 'google',
+          accessToken: tokens.access_token,
+          idToken: tokens.id_token,
+          expiresIn: 3600,
+          totp: {
+            code: acsPayload,
+          },
+        })
+      : JSON.stringify({
+          serviceName: 'google',
+          accessToken: tokens.access_token,
+          idToken: tokens.id_token,
+          expiresIn: 3600,
+          scope: 'profile',
+        });
+
     try {
       const req = await fetch(`${this.host}/api/v1/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          serviceName: 'google',
-          accessToken: tokens.access_token,
-          idToken: tokens.id_token,
-          expiresIn: 3600,
-        }),
+        body: payload,
       });
       const response = await req.json();
+
       if (response.status === 'success') {
         this.setCookies({
           rc_token: response.data.authToken,
@@ -54,6 +74,33 @@ export default class RocketChatInstance {
         }
         return { status: response.status, me: response.data.me };
       }
+
+      if (response.error === 'totp-required') {
+        return response;
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async resend2FA(emailOrUsername) {
+    try {
+      const response = await fetch(
+        `${this.host}/api/v1/users.2fa.sendEmailCode`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Auth-Token': null,
+            'X-User-Id': null,
+          },
+          mode: 'cors',
+          method: 'POST',
+          body: JSON.stringify({
+            emailOrUsername,
+          }),
+        }
+      );
+      return await response.json();
     } catch (err) {
       console.error(err.message);
     }
