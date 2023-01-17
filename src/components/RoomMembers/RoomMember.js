5fc64c28f70970394d277e6243718600e539cc8d
imf-ali <alifahad2210@gmail.com>
2023-01-17 17:13:04 +0000

    new components created

---
+++ src/components/RoomMembers/RoomMember.js
@@ -0,0 +1,46 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Icon } from '@rocket.chat/fuselage';
+import RoomMemberItem from './RoomMemberItem';
+import classes from './RoomMember.module.css';
+import { useMemberStore } from '../../store';
+
+const RoomMembers = ({ members }) => {
+  const showMembersHandler = useMemberStore(
+    (state) => state.showMembersHandler
+  );
+
+  const showChannelMembers = async (event) => {
+    showMembersHandler(false);
+  };
+  return (
+    <div className={classes.modal}>
+      <Box
+        data-qa-id="RoomHeader-Members"
+        display="flex"
+        alignItems="center"
+        height="56px"
+        is="h3"
+        pi="x24"
+        borderBlockEndWidth="default"
+        borderBlockColor="extra-light"
+        flexShrink={0}
+      >
+        <Icon name="members" pi="x2" size="x24" />
+        <Box>Members</Box>
+        <div className={classes.close} onClick={showChannelMembers}>
+          x
+        </div>
+      </Box>
+      {members.map((member, index) => (
+        <RoomMemberItem user={member} key={index} />
+      ))}
+    </div>
+  );
+};
+export default RoomMembers;
+
+RoomMembers.propTypes = {
+  members: PropTypes.arrayOf(PropTypes.shape),
+};
