4cf0a6e7f1fefd260031ba6897a951260752a8f8
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-23 20:31:51 +0530

    feat(auth): :sparkles: made Modal For TOTP

--- src/components/UI/TwoFactorTotpModal.js
+++ src/components/UI/TwoFactorTotpModal.js
@@ -1,53 +1,40 @@
 /* eslint-disable react/prop-types */
-import { Modal, Button, PasswordInput } from '@rocket.chat/fuselage';
+import { Button, PasswordInput, Box, Tile } from '@rocket.chat/fuselage';
 import React, { useState } from 'react';
+import { useUserStore } from '../../store';
 
-const TwoFactorTotpModal = ({
-  handleLogin,
-  isModalOpen,
-  setIsModalOpen,
-  method,
-}) => {
+export default function TotpModal({ handleLogin }) {
   const [accessCode, setAccessCode] = useState(null);
-
-  const handleModalToggle = () => {
-    setIsModalOpen(!isModalOpen);
-  };
+  const isUserAuthenticated = useUserStore(
+    (state) => state.isUserAuthenticated
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleLogin(accessCode);
     setAccessCode(undefined);
-    handleModalToggle();
   };
 
   const handleEdit = (e) => {
     setAccessCode(e.target.value);
   };
-  return (
-    isModalOpen && (
-      <Modal>
-        <Modal.Header>
-          <Modal.HeaderText>
-            <Modal.Title>Two-factor Authentication via ${method}</Modal.Title>
-          </Modal.HeaderText>
-          <Modal.Close />
-        </Modal.Header>
-        <Modal.Content>
-          Open your authentication app and enter the code.
-        </Modal.Content>
-        <Modal.Footer>
-          <Modal.FooterControllers>
-            <PasswordInput placeholder="123456" onChange={handleEdit} />
-            <Button onClick={handleModalToggle}>Cancel</Button>
-            <Button primary onClick={handleSubmit}>
-              Submit
-            </Button>
-          </Modal.FooterControllers>
-        </Modal.Footer>
-      </Modal>
-    )
-  );
-};
-
-export default TwoFactorTotpModal;
+  return !isUserAuthenticated ? (
+    <Box
+      zIndex="2"
+      textAlign="center"
+      position="absolute"
+      display="inline-block"
+      pi="x800"
+    >
+      <Tile elevation="2" margin={25}>
+        <form onSubmit={handleSubmit}>
+          <Box>Enter TOTP</Box>
+          <PasswordInput onChange={handleEdit} placeholder={123456} />
+          <Button onClick={handleSubmit} primary>
+            Submit
+          </Button>
+        </form>
+      </Tile>
+    </Box>
+  ) : null;
+}
