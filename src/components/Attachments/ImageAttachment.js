2a3fc149103c2347023fbaba058ca302ed9e6c29
Syed Aman Ali <aliamaan119@gmail.com>
2023-01-27 11:10:06 +0530

    fix:image not display (#99)
    * fix:image not display
    
    * Fix:made host dynamic,remove trailing slash from host

--- src/components/Attachments/ImageAttachment.js
+++ src/components/Attachments/ImageAttachment.js
@@ -2,21 +2,21 @@ import { Box } from '@rocket.chat/fuselage';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ImageAttachment = ({ attachment }) => {
-  return (
-    <Box>
-      <p>{attachment?.description}</p>
-      <img
-        src={'http://localhost:3000' + attachment.image_url}
-        height={attachment.image_dimensions.height}
-        width={attachment.image_dimensions.width}
-      />
-    </Box>
-  );
-};
+const ImageAttachment = ({ attachment, host }) => (
+  <Box>
+    <p>{attachment?.description}</p>
+    <img
+      src={host + attachment.image_url}
+      height={attachment?.image_dimensions?.height ?? 357}
+      width={attachment?.image_dimensions?.width ?? 476}
+      style={{ maxWidth: '100%', objectFit: 'cover' }}
+    />
+  </Box>
+);
 
 export default ImageAttachment;
 
 ImageAttachment.propTypes = {
   attachment: PropTypes.object,
+  host: PropTypes.string,
 };
