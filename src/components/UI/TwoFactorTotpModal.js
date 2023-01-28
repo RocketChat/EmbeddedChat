2523a852308a8cc31ddff6a36172b93e00e2690e
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-28 16:32:34 +0530

    feat(auth): :construction: add a cancel button

--- src/components/UI/TwoFactorTotpModal.js
+++ src/components/UI/TwoFactorTotpModal.js
@@ -1,8 +1,8 @@
-/* eslint-disable react/prop-types */
 import { Button, PasswordInput, Box, Tile, Flex } from '@rocket.chat/fuselage';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useUserStore, useTotpStore } from '../../store';
+import styles from './TwoFactorTotpModal.module.css';
 
 export default function TotpModal({ handleLogin }) {
   const [accessCode, setAccessCode] = useState(null);
@@ -10,39 +10,51 @@ export default function TotpModal({ handleLogin }) {
     (state) => state.isUserAuthenticated
   );
   const isModalOpen = useTotpStore((state) => state.isModalOpen);
+  const SetisModalOpen = useTotpStore((state) => state.SetisModalOpen);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleLogin(accessCode);
     setAccessCode(undefined);
   };
+  const handleClose = () => {
+    SetisModalOpen(false);
+  };
 
   const handleEdit = (e) => {
     setAccessCode(e.target.value);
   };
   return isModalOpen ? (
-    <Box
-      zIndex="2"
-      textAlign="center"
-      position="absolute"
-      display="inline-block"
-      pi="x250"
-    >
-      <Flex.Container wrap="wrap" alignContent="end">
-        <Tile elevation="2" margin={25}>
-          <form onSubmit={handleSubmit}>
-            <Box>Enter TOTP</Box>
-            <PasswordInput onChange={handleEdit} placeholder={123456} />
-            <Button onClick={handleSubmit} primary>
-              Submit
-            </Button>
-          </form>
-        </Tile>
-      </Flex.Container>
-    </Box>
+    <>
+      <div className={styles.modalcontainer}>
+        <Box
+          zIndex="2"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          position="absolute"
+          m="x30"
+          width="x150"
+          height="x120"
+        >
+          <Tile elevation="2" margin={25}>
+            <form onSubmit={handleSubmit}>
+              <Box>Enter TOTP</Box>
+              <PasswordInput onChange={handleEdit} placeholder={123456} />
+              <Flex.Container justifyContent="space-around">
+                <Button onClick={handleSubmit} primary>
+                  Submit
+                </Button>
+                <Button onClick={handleClose}>Close</Button>
+              </Flex.Container>
+            </form>
+          </Tile>
+        </Box>
+      </div>
+    </>
   ) : null;
 }
 
-TotpModal.prototype = {
+TotpModal.propTypes = {
   handleLogin: PropTypes.func,
 };
