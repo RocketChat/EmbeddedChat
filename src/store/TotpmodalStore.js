a825a4708a7325916123fd2a273172979b6bd853
imvipindev <vipinchaudhary1809@gmail.com>
2023-01-30 17:20:37 +0530

    refactor(auth): :fire: remnamed totpmodalstore

--- src/store/TotpmodalStore.js
+++
@@ -1,8 +0,0 @@
-import create from 'zustand';
-
-const useTotpStore = create((set) => ({
-  isModalOpen: false,
-  SetisModalOpen: (isModalOpen) => set(() => ({ isModalOpen })),
-}));
-
-export default useTotpStore;
