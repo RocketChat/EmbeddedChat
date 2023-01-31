8922f1e3d2bb46c3fe69ab23a0515ef7129ff829
Shreekrishna Bhat <80944218+SKB231@users.noreply.github.com>
2023-01-31 13:53:31 -0500

    Added Report Message Functionality (#114)
    * Added report message window component, and corresponding CSS file.
    
    * Completed front end work: Added pop up window with backdrop. Just have to make the API call and add cancel button
    
    * Finished adding the API call

--- src/store/messageStore.js
+++ src/store/messageStore.js
@@ -4,9 +4,16 @@ const useMessageStore = create((set) => ({
   messages: [],
   filtered: false,
   editMessage: {},
+  messageToReport: NaN,
+  showReportMessage: false,
   setFilter: (filter) => set((state) => ({ ...state, filtered: filter })),
   setMessages: (messages) => set((state) => ({ ...state, messages })),
   setEditMessage: (editMessage) => set(() => ({ editMessage })),
+  setMessageToReport: (messageId) =>
+    set((state) => ({ ...state, messageToReport: messageId })),
+  toggleShowReportMessage: () => {
+    set((state) => ({ ...state, showReportMessage: !state.showReportMessage }));
+  },
 }));
 
 export default useMessageStore;
