913a23172d5c2cb5aa58e737e08daec31769305b
Abhinav Kumar <abhinav@avitechlab.com>
2023-09-24 14:03:49 +0530

    svg icon components
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

---
+++ packages/react/src/components/Icon/icons/index.js
@@ -0,0 +1,73 @@
+import File from './File';
+import Mobile from './Mobile';
+import Star from './Star';
+import Pin from './Pin';
+import ReplyDirectly from './ReplyDirectly';
+import Hash from './Hash';
+import Computer from './Computer';
+import Cross from './Cross';
+import Mic from './Mic';
+import CircleCross from './CircleCross';
+import CircleCheck from './CircleCheck';
+import Send from './Send';
+import Emoji from './Emoji';
+import Plus from './Plus';
+import Thread from './Thread';
+import User from './User';
+import Clock from './Clock';
+import Back from './Back';
+import Report from './Report';
+import Info from './Info';
+import Members from './Members';
+import Link from './Link';
+import Magnifier from './Magnifier';
+import Edit from './Edit';
+import ArrowBack from './ArrowBack';
+import Google from './Google';
+import Multiline from './Multiline';
+import Code from './Code';
+import Strike from './Strike';
+import Bold from './Bold';
+import Italic from './Italic';
+import StarFilled from './StarFilled';
+import Trash from './Trash';
+import Kebab from './Kebab';
+
+const icons = {
+  file: File,
+  mobile: Mobile,
+  star: Star,
+  pin: Pin,
+  'reply-directly': ReplyDirectly,
+  hash: Hash,
+  computer: Computer,
+  cross: Cross,
+  mic: Mic,
+  'circle-cross': CircleCross,
+  'circle-check': CircleCheck,
+  send: Send,
+  emoji: Emoji,
+  plus: Plus,
+  thread: Thread,
+  user: User,
+  clock: Clock,
+  back: Back,
+  report: Report,
+  info: Info,
+  members: Members,
+  link: Link,
+  magnifier: Magnifier,
+  edit: Edit,
+  'arrow-back': ArrowBack,
+  google: Google,
+  multiline: Multiline,
+  code: Code,
+  strike: Strike,
+  bold: Bold,
+  italic: Italic,
+  'star-filled': StarFilled,
+  trash: Trash,
+  kebab: Kebab,
+};
+
+export default icons;
---
+++ packages/react/tools/icons-generator.js
@@ -0,0 +1,105 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const iconsList = [
+  'file',
+  'mobile',
+  'star',
+  'pin',
+  'reply-directly',
+  'hash',
+  'computer',
+  'cross',
+  'mic',
+  'circle-cross',
+  'circle-check',
+  'send',
+  'emoji',
+  'plus',
+  'thread',
+  'user',
+  'clock',
+  'back',
+  'report',
+  'info',
+  'members',
+  'link',
+  'magnifier',
+  'edit',
+  'arrow-back',
+  'google',
+  'multiline',
+  'code',
+  'strike',
+  'bold',
+  'italic',
+  'star-filled',
+  'trash',
+  'kebab',
+];
+const svgDirPath = path.join(
+  __dirname,
+  '../../../node_modules/@rocket.chat/icons/dist/svg'
+);
+
+const svgIconOutputDir = path.join(__dirname, '../src/components/Icon/icons');
+
+const camelCase = (name) =>
+  name
+    .split('-')
+    .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+    .join('');
+
+const codeModifier = (code) => {
+  let newCode = code.replace(/class=/g, 'className=');
+  const openingTag = newCode.match(/<svg.*>/g)[0];
+  const newOpeningTag = openingTag.replace('>', ' {...props}>');
+  newCode = newCode.replace(openingTag, newOpeningTag);
+  return newCode;
+};
+
+const getComponentCode = (name, svgData) => `
+    import React from 'react';
+
+    const ${camelCase(name)} = (props) => (
+      ${codeModifier(svgData)}
+    )
+
+    export default ${camelCase(name)};
+  `;
+
+const getIndexFileCode = () => {
+  const importCodes = iconsList
+    .map((icon) => `import ${camelCase(icon)} from './${camelCase(icon)}';`)
+    .join('\n');
+  const exportCodes = iconsList
+    .map((icon) => `"${icon}": ${camelCase(icon)}`)
+    .join(',\n');
+  return `
+    ${importCodes}
+
+    const icons = {
+      ${exportCodes}
+    };
+
+    export default icons;
+  `;
+};
+
+if (!fs.existsSync(svgIconOutputDir)) {
+  fs.mkdirSync(svgIconOutputDir, { recursive: true });
+}
+iconsList.forEach((icon) => {
+  const svgIconPath = path.join(svgDirPath, `${icon}.svg`);
+  const svgIconOutputPath = path.join(
+    svgIconOutputDir,
+    `${camelCase(icon)}.js`
+  );
+  const svgData = fs.readFileSync(svgIconPath).toString();
+  const componentCode = getComponentCode(icon, svgData);
+  fs.writeFileSync(svgIconOutputPath, componentCode);
+});
+
+fs.writeFileSync(path.join(svgIconOutputDir, 'index.js'), getIndexFileCode());
+execSync(`npx prettier --write '${svgIconOutputDir}' --loglevel=silent`);
