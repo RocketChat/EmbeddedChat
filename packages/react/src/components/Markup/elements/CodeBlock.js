47a3c4413aa68f65ae304bf26dcce44ccbc07119
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-10-04 19:44:56 +0530

    emotion-CodeBlock

--- packages/react/src/components/Markup/elements/CodeBlock.js
+++ packages/react/src/components/Markup/elements/CodeBlock.js
@@ -1,6 +1,22 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import styles from './CodeBlock.module.css';
+import { css } from '@emotion/react'; // Step 1: Import `css` from Emotion.sh
+
+const styles = {
+  copyonly: css`
+    display: none;
+    width: 100%;
+    height: 0;
+    user-select: none;
+    vertical-align: baseline;
+    font-size: 0;
+    -moz-box-orient: vertical;
+  `,
+  prestyle: css`
+    display: inline-block;
+    width: 100%;
+  `,
+};
 
 const CodeBlock = ({ lines }) => {
   const code = useMemo(
@@ -9,10 +25,10 @@ const CodeBlock = ({ lines }) => {
   );
 
   return (
-    <pre role="region" className={styles.prestyle}>
-      <span className={styles.copyonly}>```</span>
+    <pre role="region" css={styles.prestyle}>
+      <span css={styles.copyonly}>```</span>
       <code>{code}</code>
-      <span className={styles.copyonly}>```</span>
+      <span css={styles.copyonly}>```</span>
     </pre>
   );
 };
