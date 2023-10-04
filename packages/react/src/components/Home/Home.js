1c99a9642c60f018369809c092051b00c76adf13
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-10-04 19:04:23 +0530

    emotion-home

--- packages/react/src/components/Home/Home.js
+++ packages/react/src/components/Home/Home.js
@@ -4,16 +4,33 @@ import React from 'react';
 import { Box } from '../Box';
 import styles from './Home.module.css';
 
+// Define Emotion.sh styles for the component
+const componentStyles = {
+  container: css`
+    height: ${height};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+  paraText: css`
+    margin-top: 1rem;
+    font-size: 1rem;
+
+    @media (max-width: 768px) {
+      margin-top: 0.5rem;
+      font-size: 0.775rem;
+      line-height: 1rem;
+    }
+
+    > code {
+      font-size: inherit;
+    }
+  `,
+};
+
 const Home = ({ height }) => (
-  <Box
-    css={css`
-      height: ${height};
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    `}
-  >
+  <Box css={componentStyles.container}>
     <h2 style={{ display: 'inline' }}>EmbeddedChat | Home</h2>
     <p>
       For more documentation visit:{' '}
@@ -25,11 +42,11 @@ const Home = ({ height }) => (
         README
       </a>
     </p>
-    <p className={styles.paraText}>
+    <p css={componentStyles.paraText}>
       To enable anonymous mode pass in{' '}
       <strong>anonymousMode prop as true</strong> and, then
     </p>
-    <p className={styles.paraText}>
+    <p css={componentStyles.paraText}>
       Enable Anonymous read by going to -&gt;{' '}
       <code>Admin -&gt; Accounts -&gt; Allow Anonymous Read</code>. This will
       allow users to see the chat without logging in.
