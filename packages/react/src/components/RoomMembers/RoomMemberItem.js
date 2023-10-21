55186559de5242fff722b3b82b14cedcf92b5a76
Abhinav Kumar <abhinav@avitechlab.com>
2023-10-21 22:32:17 +0530

    lint fixes (#285)
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/react/src/components/RoomMembers/RoomMemberItem.js
+++ packages/react/src/components/RoomMembers/RoomMemberItem.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Option,
-  OptionColumn,
-  OptionDescription,
-  OptionContent,
-} from '@rocket.chat/fuselage';
-import UserStatus from './UserStatus';
 
 const RoomMemberItem = ({ user }) => (
-  
-  <div style={{ width:'100%',paddingBottom:'8px',paddingTop:'8px',display:'flex' }}>
-    <img src={ `http://localhost:3000/avatar/${user.username}`} alt="avatar" style={{ height:'1.5rem',marginLeft:'0.5rem',}}></img>
-    <span style={{ marginLeft:'1.2rem',}}>{user.username}</span>
+  <div
+    style={{
+      width: '100%',
+      paddingBottom: '8px',
+      paddingTop: '8px',
+      display: 'flex',
+    }}
+  >
+    <img
+      src={`http://localhost:3000/avatar/${user.username}`}
+      alt="avatar"
+      style={{ height: '1.5rem', marginLeft: '0.5rem' }}
+    />
+    <span style={{ marginLeft: '1.2rem' }}>{user.username}</span>
   </div>
 );
 
