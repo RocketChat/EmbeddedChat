0e639cd86842a4049ef6a878e47106bb92195a46
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-10-22 12:22:48 +0530

    search-message-mods

--- packages/react/src/components/SearchMessage/SearchMessage.js
+++ packages/react/src/components/SearchMessage/SearchMessage.js
@@ -7,7 +7,7 @@ import { Button } from '../Button';
 import { Box } from '../Box';
 import { Icon } from '../Icon';
 import { ActionButton } from '../ActionButton';
-import { Message } from '../Message';
+import { Message } from '../Message/Message';
 import MessageDivider from '../Message/MessageDivider';
 import { MessageReactions } from '../Message/MessageReactions';
 
