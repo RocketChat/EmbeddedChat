eaa03e9ab8845cc2fbe510d0ad62164ce3298bc1
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-10-27 19:50:40 +0530

    emotion-GenericModal

--- packages/react/src/components/GenericModal/GenericModal.js
+++ packages/react/src/components/GenericModal/GenericModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from './Generic.module.css';
+import { css } from '@emotion/react';
 import { Icon } from '../Icon';
 import { Modal } from '../Modal';
 
@@ -27,14 +27,29 @@ const renderIcon = (icon, variant) => {
   return icon;
 };
 
+const closeStyles = css`
+  cursor: pointer;
+  padding: 12px 16px;
+
+  &:hover {
+    background: #bbb;
+  }
+`;
+
+const modalFooterStyles = css`
+  display: flex;
+  justify-content: space-around;
+`;
+
 const GenericModal = ({ variant = 'info', children, title, icon, onClose }) => (
   <Modal>
     <Modal.Header>
       {renderIcon(icon, variant)}
       <Modal.Title>{title ?? 'Are_you_sure'}</Modal.Title>
-      <Modal.Close className={classes.close} title="Close" onClick={onClose} />
+      <Modal.Close css={closeStyles} title="Close" onClick={onClose} />
     </Modal.Header>
     <Modal.Content>{children}</Modal.Content>
+    <div css={modalFooterStyles}>{/* Footer content */}</div>
   </Modal>
 );
 
