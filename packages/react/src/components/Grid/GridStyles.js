a52cb2edc0e369aba6be439ee22e5745877ffac4
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-10-28 12:06:49 +0530

    emotion-styles-grid

--- packages/react/src/components/Grid/GridStyles.js
+++ packages/react/src/components/Grid/GridStyles.js
@@ -1,17 +1,23 @@
-import { css } from '@emotion/react';
+// import { css } from '@emotion/react';
 
-// Define Reusable CSS constants
-const toRem = (pixels) => `${pixels / 16}rem`;
+// // Define Reusable CSS constants
+// const toRem = (pixels) => `${pixels / 16}rem`;
 
-// Emotion styles
-const styles = {
-  ecGrid: css`
-    display: flex;
-    flex-flow: row wrap;
-    margin-block: calc(${toRem(16)} / -2);
-    margin-inline: calc(${toRem(16)} / -2);
-  `,
-  ecGridWrapper: css`
-    overflow: hidden;
-  `,
-};
+// // Emotion styles
+// const styles = {
+//   ecGrid: css`
+//     display: flex;
+//     flex-flow: row wrap;
+//     margin-block: calc(${toRem(16)} / -2);
+//     margin-inline: calc(${toRem(16)} / -2);
+//   `,
+//   ecGridWrapper: css`
+//     overflow: hidden;
+//   `,
+//   ecGridXs: css`
+//     '& > .ec-grid': {
+//       marginBlock: calc(${toRem(16)} / -2);
+//       marginInline: calc(${toRem(16)} / -2);
+//     }
+//   `,
+// };
