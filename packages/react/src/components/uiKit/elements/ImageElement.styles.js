a252617974ee0e11a2345b51d13d37794dee905a
Abhinav Kumar <abhinav@avitechlab.com>
2023-11-18 00:12:44 +0530

    fix ImageElement.styles.js
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/react/src/components/uiKit/elements/ImageElement.styles.js
+++ packages/react/src/components/uiKit/elements/ImageElement.styles.js
@@ -1,18 +1,19 @@
-import styled from '@rocket.chat/styled';
+import { css } from '@emotion/react';
+import React from 'react';
 
-const filterElementProps = ({ imageUrl: _imageUrl, size: _size, ...props }) =>
-  props;
-
-export const Element = styled('div', filterElementProps)`
-  box-shadow: 0 0 0px 1px rgba(204, 204, 204, 38%);
-  background-repeat: no-repeat;
-  background-position: 50%;
-  background-size: cover;
-  background-color: rgba(204, 204, 204, 38%);
-  background-image: url(${(props) => props.imageUrl});
-  width: ${(props) => String(props.size)}px;
-  height: ${(props) => String(props.size)}px;
-  border-radius: 4px;
-  overflow: hidden;
-  margin-inline-start: 4px;
-`;
+export const Element = ({ imageUrl, size }) => {
+  const ElementCss = css`
+    box-shadow: 0 0 0px 1px rgba(204, 204, 204, 38%);
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: cover;
+    background-color: rgba(204, 204, 204, 38%);
+    background-image: url(${imageUrl});
+    width: ${String(size)}px;
+    height: ${String(size)}px;
+    border-radius: 4px;
+    overflow: hidden;
+    margin-inline-start: 4px;
+  `;
+  return <div css={ElementCss} />;
+};
