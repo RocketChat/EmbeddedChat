c98bc0ad0ea5a346aa6930b91ee44226f1dc0649
Abhinav Kumar <abhinav@avitechlab.com>
2023-11-17 16:41:55 +0530

    remove unused packages and fuselage
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/react/src/components/uiKit/elements/StaticSelectElement.js
+++ packages/react/src/components/uiKit/elements/StaticSelectElement.js
@@ -1,43 +1,43 @@
-import { SelectFiltered } from '@rocket.chat/fuselage';
-import React, { memo, useCallback, useMemo } from 'react';
+// import { SelectFiltered } from '@rocket.chat/fuselage';
+// import React, { memo, useCallback, useMemo } from 'react';
 
-import { useUiKitState } from '../hooks/useUiKitState';
-import { fromTextObjectToString } from '../utils/fromTextObjectToString';
+// import { useUiKitState } from '../hooks/useUiKitState';
+// import { fromTextObjectToString } from '../utils/fromTextObjectToString';
 
-const StaticSelectElement = ({ block, context, surfaceRenderer }) => {
-  const [{ loading, value, error }, action] = useUiKitState(block, context);
+// const StaticSelectElement = ({ block, context, surfaceRenderer }) => {
+//   const [{ loading, value, error }, action] = useUiKitState(block, context);
 
-  const options = useMemo(
-    () =>
-      block.options.map((option, i) => [
-        option.value,
-        fromTextObjectToString(surfaceRenderer, option.text, i) ?? '',
-      ]),
-    [block.options, surfaceRenderer]
-  );
+//   const options = useMemo(
+//     () =>
+//       block.options.map((option, i) => [
+//         option.value,
+//         fromTextObjectToString(surfaceRenderer, option.text, i) ?? '',
+//       ]),
+//     [block.options, surfaceRenderer]
+//   );
 
-  const handleChange = useCallback(
-    // eslint-disable-next-line no-shadow
-    (value) => {
-      action({ target: { value } });
-    },
-    [action]
-  );
+//   const handleChange = useCallback(
+//     // eslint-disable-next-line no-shadow
+//     (value) => {
+//       action({ target: { value } });
+//     },
+//     [action]
+//   );
 
-  return (
-    <SelectFiltered
-      value={value}
-      disabled={loading}
-      error={error}
-      options={options}
-      placeholder={fromTextObjectToString(
-        surfaceRenderer,
-        block.placeholder,
-        0
-      )}
-      onChange={handleChange}
-    />
-  );
-};
+//   return (
+//     <SelectFiltered
+//       value={value}
+//       disabled={loading}
+//       error={error}
+//       options={options}
+//       placeholder={fromTextObjectToString(
+//         surfaceRenderer,
+//         block.placeholder,
+//         0
+//       )}
+//       onChange={handleChange}
+//     />
+//   );
+// };
 
-export default memo(StaticSelectElement);
+// export default memo(StaticSelectElement);
--- packages/react/src/components/uiKit/surfaces/FuselageSurfaceRenderer.js
+++ packages/react/src/components/uiKit/surfaces/FuselageSurfaceRenderer.js
@@ -6,16 +6,16 @@ import ContextBlock from '../blocks/ContextBlock';
 import DividerBlock from '../blocks/DividerBlock';
 import ImageBlock from '../blocks/ImageBlock';
 import InputBlock from '../blocks/InputBlock';
-import PreviewBlock from '../blocks/PreviewBlock';
+// import PreviewBlock from '../blocks/PreviewBlock';
 import SectionBlock from '../blocks/SectionBlock';
 import ButtonElement from '../elements/ButtonElement';
 import DatePickerElement from '../elements/DatePickerElement';
 import ImageElement from '../elements/ImageElement';
 import LinearScaleElement from '../elements/LinearScaleElement';
-import MultiStaticSelectElement from '../elements/MultiStaticSelectElement';
-import OverflowElement from '../elements/OverflowElement';
+// import MultiStaticSelectElement from '../elements/MultiStaticSelectElement';
+// import OverflowElement from '../elements/OverflowElement';
 import PlainTextInputElement from '../elements/PlainTextInputElement';
-import StaticSelectElement from '../elements/StaticSelectElement';
+// import StaticSelectElement from '../elements/StaticSelectElement';
 import { Markup } from '../../Markup';
 
 export class FuselageSurfaceRenderer extends UiKit.SurfaceRenderer {
@@ -79,15 +79,18 @@ export class FuselageSurfaceRenderer extends UiKit.SurfaceRenderer {
     if (context !== UiKit.BlockContext.BLOCK) {
       return null;
     }
-    return (
-      <PreviewBlock
-        key={index}
-        block={block}
-        context={context}
-        index={index}
-        surfaceRenderer={this}
-      />
-    );
+    return null;
+
+    // TODO: Implement this without fuselage.
+    // return (
+    //   <PreviewBlock
+    //     key={index}
+    //     block={block}
+    //     context={context}
+    //     index={index}
+    //     surfaceRenderer={this}
+    //   />
+    // );
   }
 
   context(block, context, index) {
@@ -215,15 +218,17 @@ export class FuselageSurfaceRenderer extends UiKit.SurfaceRenderer {
       return null;
     }
 
-    return (
-      <StaticSelectElement
-        key={block.actionId || index}
-        block={block}
-        context={context}
-        index={index}
-        surfaceRenderer={this}
-      />
-    );
+    return null;
+    // implement this without fuselage
+    // return (
+    //   <StaticSelectElement
+    //     key={block.actionId || index}
+    //     block={block}
+    //     context={context}
+    //     index={index}
+    //     surfaceRenderer={this}
+    //   />
+    // );
   }
 
   multi_static_select(block, context, index) {
@@ -231,15 +236,17 @@ export class FuselageSurfaceRenderer extends UiKit.SurfaceRenderer {
       return null;
     }
 
-    return (
-      <MultiStaticSelectElement
-        key={block.actionId || index}
-        block={block}
-        context={context}
-        index={index}
-        surfaceRenderer={this}
-      />
-    );
+    return null;
+    // implement this without fuselage
+    // return (
+    //   <MultiStaticSelectElement
+    //     key={block.actionId || index}
+    //     block={block}
+    //     context={context}
+    //     index={index}
+    //     surfaceRenderer={this}
+    //   />
+    // );
   }
 
   overflow(block, context, index) {
@@ -247,15 +254,17 @@ export class FuselageSurfaceRenderer extends UiKit.SurfaceRenderer {
       return null;
     }
 
-    return (
-      <OverflowElement
-        key={index}
-        block={block}
-        context={context}
-        index={index}
-        surfaceRenderer={this}
-      />
-    );
+    return null;
+    // implement this without fuselage
+    // return (
+    //   <OverflowElement
+    //     key={index}
+    //     block={block}
+    //     context={context}
+    //     index={index}
+    //     surfaceRenderer={this}
+    //   />
+    // );
   }
 
   plain_text_input(block, context, index) {
