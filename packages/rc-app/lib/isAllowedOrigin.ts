ee1b05fe9c8ced001771ab8a1caf44490ab2be80
Abhinav Kumar <abhinav@avitechlab.com>
2023-11-19 23:03:20 +0530

    added info endpoint to rc-apps
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/rc-app/lib/isAllowedOrigin.ts
+++ packages/rc-app/lib/isAllowedOrigin.ts
@@ -1,14 +1,11 @@
 import { IRead } from "@rocket.chat/apps-engine/definition/accessors";
+import { getAllowedOrigins } from "./getAllowedOrigins";
 
 export const isAllowedOrigin = async (read: IRead, origin: string) => {
-    const allowedDomains = await read.getEnvironmentReader().getSettings().getValueById("allowed-domains");
-    if (!allowedDomains) {
-        return true;
-    }
-    const domains = allowedDomains.split(',').filter((domain) => !!domain.trim());
-    if (domains.length === 0) {
+    const allowedOrigins = await getAllowedOrigins(read);
+    if (allowedOrigins.length === 0) {
         // all origins are allowed
         return true;
     }
-    return domains.includes(origin);
+    return allowedOrigins.includes(origin);
 }
