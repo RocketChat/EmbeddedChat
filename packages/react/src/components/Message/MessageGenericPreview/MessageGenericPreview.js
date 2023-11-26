93b8d36452fd12a113605374f14c2cda5f34d8bb
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-11-26 14:36:17 +0530

    MessageGenericPreview.js

---
+++ packages/react/src/components/Message/MessageGenericPreview/MessageGenericPreview.js
@@ -0,0 +1,9 @@
+// MessageGenericPreview.js
+import React from 'react';
+import './MessageGenericPreview.styles.scss';
+
+const MessageGenericPreview = (props) => {
+  return <div className="rcx-message-generic-preview" {...props} />;
+};
+
+export default MessageGenericPreview;
