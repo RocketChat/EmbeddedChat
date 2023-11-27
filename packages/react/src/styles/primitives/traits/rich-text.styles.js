bf98be81cea933b4b3990fbac422f2b9cb50d59f
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-11-27 22:45:47 +0530

    emotion-styling2

---
+++ packages/react/src/styles/primitives/traits/rich-text.styles.js
@@ -0,0 +1,86 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { colors, lengths, typography } from '../../your-token-paths'; // Update with your actual paths
+
+const withInlineElements = css`
+  a,
+  abbr,
+  b,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  i,
+  ins,
+  kbd,
+  q,
+  samp,
+  small,
+  strong,
+  sub,
+  sup,
+  time,
+  var {
+    letter-spacing: inherit;
+    color: inherit;
+    font: inherit;
+  }
+
+  a {
+    &:link,
+    &:visited,
+    &.is-visited,
+    &:active,
+    &.is-active {
+      color: ${colors.font.info};
+    }
+
+    &:hover,
+    &.is-hovered {
+      text-decoration: underline;
+    }
+  }
+
+  // ... (other styles)
+
+  time {
+    letter-spacing: inherit;
+    color: ${colors.font.secondaryInfo};
+    font: inherit;
+  }
+
+  // ... (other styles)
+
+  small {
+    letter-spacing: inherit;
+    font: inherit;
+    font-size: 80%;
+  }
+`;
+
+const withBlockElements = css`
+  font: inherit;
+
+  h1 {
+    ${typography.useFontScale('h2')};
+  }
+
+  h2 {
+    ${typography.useFontScale('h4')};
+  }
+
+  h3 {
+    ${typography.useFontScale('p2m')};
+  }
+
+  // ... (other styles)
+`;
+
+// Example usage:
+const myStyledComponent = css`
+  ${withInlineElements};
+  ${withBlockElements};
+`;
+
+// Apply myStyledComponent to your component
