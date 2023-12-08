c3eb5400c5a9c850639b2b3e72fe49fc1425cdfc
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-12-08 12:05:19 +0530

    remove-useContext()

--- packages/react/src/components/Message/MessageToolbox.js
+++ packages/react/src/components/Message/MessageToolbox.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/react';
 import Popup from 'reactjs-popup';
 import useComponentOverrides from '../../theme/useComponentOverrides';
