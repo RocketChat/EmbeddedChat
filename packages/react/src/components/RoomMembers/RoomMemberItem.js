c8c9dee09a69ca85b8bb9cde7a2a1bd0cb7fff58
Umang Utkarsh <95426993+umangutkarsh@users.noreply.github.com>
2023-12-13 16:16:21 +0530

    Fix/#334 source image fix (#335)
    * added-babel-config
    
    * mods
    
    * changed-host-to-dynamic
    
    * construct-source-using-host
    
    * used URL object to create url using host as base
    
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>
    
    * added changesets
    
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>
    
    ---------
    
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>
    Co-authored-by: Abhinav Kumar <abhinav@avitechlab.com>

--- packages/react/src/components/RoomMembers/RoomMemberItem.js
+++ packages/react/src/components/RoomMembers/RoomMemberItem.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RoomMemberItem = ({ user }) => (
-  <div
-    style={{
-      width: '100%',
-      paddingBottom: '8px',
-      paddingTop: '8px',
-      display: 'flex',
-    }}
-  >
-    <img
-      src={`http://localhost:3000/avatar/${user.username}`}
-      alt="avatar"
-      style={{ height: '1.5rem', marginLeft: '0.5rem' }}
-    />
-    <span style={{ marginLeft: '1.2rem' }}>{user.username}</span>
-  </div>
-);
+const RoomMemberItem = ({ user, host }) => {
+  const avatarUrl = new URL(`avatar/${user.username}`, host).toString();
+  return (
+    <div
+      style={{
+        width: '100%',
+        paddingBottom: '8px',
+        paddingTop: '8px',
+        display: 'flex',
+      }}
+    >
+      <img
+        src={avatarUrl}
+        alt="avatar"
+        style={{ height: '1.5rem', marginLeft: '0.5rem' }}
+      />
+      <span style={{ marginLeft: '1.2rem' }}>{user.username}</span>
+    </div>
+  );
+};
 
 export default RoomMemberItem;
 
