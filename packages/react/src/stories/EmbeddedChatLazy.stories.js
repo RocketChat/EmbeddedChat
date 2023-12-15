6b33ccecb81b71934ed31b0f20b9e9be7512db57
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-12-15 12:41:22 +0530

    lazyload-import-fix

--- packages/react/src/stories/EmbeddedChatLazy.stories.js
+++ packages/react/src/stories/EmbeddedChatLazy.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { EmbeddedChatLazy } from '..';
+import { EmbeddedChat } from '..';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'EmbeddedChat/LazyLoad',
-  component: EmbeddedChatLazy,
+  component: EmbeddedChat,
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
