ca31d807eda5d3cd1f0f2f8dad4dd9285de32fb7
Akshun-01 <akshunkuthiala2002@gmail.com>
2024-01-02 19:20:14 +0530

    add toggle password visiblity

--- packages/react/src/components/auth/LoginForm.js
+++ packages/react/src/components/auth/LoginForm.js
@@ -10,6 +10,7 @@ import { Input } from '../Input';
 export default function LoginForm() {
   const [userOrEmail, setuserOrEmail] = useState(null);
   const [password, setpassword] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
   const isLoginModalOpen = loginModalStore((state) => state.isLoginModalOpen);
   const setIsLoginModalOpen = loginModalStore(
     (state) => state.setIsLoginModalOpen
@@ -28,6 +29,9 @@ export default function LoginForm() {
   };
   const handleEditPassword = (e) => {
     setpassword(e.target.value);
+  };
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
   };
 
   const fieldCSS = css`
@@ -64,6 +68,12 @@ export default function LoginForm() {
     margin-block-end: 0.125rem;
   `;
 
+  const passwordEyeCss = css`
+    cursor: pointer;
+    position: absolute;
+    right: 1em;
+  `;
+
   return isLoginModalOpen ? (
     <>
       <GenericModal
@@ -87,7 +97,49 @@ export default function LoginForm() {
           <Box css={fieldCSS}>
             <Box css={fieldLabel}>Password</Box>
             <Box css={fieldRow}>
-              <Input type="password" onChange={handleEditPassword} />
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                onChange={handleEditPassword}
+              />
+              <Box
+                type="button"
+                css={passwordEyeCss}
+                onClick={handleTogglePassword}
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-eye"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-eye-off"
+                  >
+                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
+                    <line x1="1" y1="1" x2="23" y2="23" />
+                  </svg>
+                )}
+              </Box>
             </Box>
           </Box>
           <Box
