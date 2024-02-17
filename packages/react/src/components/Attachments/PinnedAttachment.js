1d28ad80ffca99f88dd866c99f2888207518f4a4
Sayan Banerjee <sayanbanerjee2002@gmail.com>
2024-02-17 11:43:16 +0530

    fix/User avatar not visible after pinning a message. #466

--- packages/react/src/components/Attachments/PinnedAttachment.js
+++ packages/react/src/components/Attachments/PinnedAttachment.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
+import { Avatar } from '../Avatar';
+import RCContext from '../../context/RCInstance';
 
-const PinnedAttachment = ({ attachment }) => (
-  <Box
-    style={{
-      borderInlineStart: '1px solid currentColor',
-      paddingLeft: '0.8rem'
-    }}
-  >
-    <Box>{attachment?.author_name}</Box>
-    <Box>{attachment?.text}</Box>
-  </Box>
-);
+const PinnedAttachment = ({ attachment }) => {
+  const { RCInstance } = useContext(RCContext);
+  const getUserAvatarUrl = (authorIcon) => {
+    const host = RCInstance.getHost();
+    const URL = `${host}${authorIcon}`;
+    return URL;
+  };
+  return (
+    <Box
+      style={{
+        borderInlineStart: '1px solid currentColor',
+        paddingLeft: '0.8rem'
+      }}
+    >
+      <Box style={{
+        display: 'flex',
+        gap: '0.5rem',
+      }}>
+        <Avatar
+          url={getUserAvatarUrl(attachment?.author_icon)}
+          alt="avatar"
+          size={'1.2em'}
+        />
+        <Box>{attachment?.author_name}</Box>
+      </Box>
+      <Box>{attachment?.text}</Box>
+    </Box>)
+}
+
 
 export default PinnedAttachment;
 
