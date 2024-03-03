b21245a0bd3c51d1b1abf8b5253c3b6cf93a7dc1
Umang Utkarsh <umangutkarsh0024@gmail.com>
2024-03-03 22:05:13 +0530

    format-fix

--- packages/react/src/components/ChatHeader/ChatHeader.js
+++ packages/react/src/components/ChatHeader/ChatHeader.js
@@ -149,7 +149,7 @@ const ChatHeader = ({
     setShowAllFiles(true);
     setShowSearch(false);
   }, [setShowAllFiles, setShowSearch]);
-  
+
   const showMentions = useCallback(async () => {
     setShowMentions(true);
     setShowSearch(false);
