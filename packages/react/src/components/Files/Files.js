0f6062680a2150c76e3dac44f3649f920d33a09e
Zishan Ahmad <zishan.barun@gmail.com>
2024-03-22 12:14:46 +0530

    fixed multiple rendering bugs
    refactored other component using sidebars

--- packages/react/src/components/Files/Files.js
+++ packages/react/src/components/Files/Files.js
@@ -14,6 +14,7 @@ import Menu from '../Menu/Menu';
 import { Modal } from '../Modal';
 import { Button } from '../Button';
 import { useToastBarDispatch } from '../../hooks/useToastBarDispatch';
+import Sidebar from '../Sidebar/Sidebar';
 
 const MessageCss = css`
   display: flex;
@@ -98,10 +99,6 @@ const Files = () => {
   const [isFilesFetched, setIsFilesFetched] = useState(false);
   const [files, setFiles] = useState([]);
   const [fileToDelete, setFileToDelete] = useState({});
-
-  const toggleShowAllFiles = () => {
-    setShowAllFiles(false);
-  };
 
   const handleInputChange = (e) => {
     setText(e.target.value);
@@ -208,127 +205,99 @@ const Files = () => {
             </Modal.Footer>
           </Modal>
         )}
-      <Box css={componentStyle}>
-        <Box css={wrapContainerStyle}>
-          <Box style={{ padding: '16px' }}>
-            <Box
-              css={css`
-                display: flex;
-              `}
-            >
-              <h3 style={{ display: 'contents' }}>
-                <Icon
-                  name="clip"
-                  size="1.25rem"
-                  style={{ padding: '0px 20px 20px 0px' }}
-                />
-                <Box
-                  css={css`
-                    width: 100%;
-                    color: #4a4a4a;
-                  `}
-                >
-                  Files
-                </Box>
-                <ActionButton onClick={toggleShowAllFiles} ghost size="small">
-                  <Icon name="cross" size="1.25rem" />
-                </ActionButton>
-              </h3>
-            </Box>
 
-            <Box css={searchContainerStyle}>
-              <input
-                placeholder="Search Files"
-                onChange={handleInputChange}
-                css={textInputStyle}
-              />
+      <Sidebar
+        title="Files"
+        iconName="attachment"
+        setShowWindow={setShowAllFiles}
+      >
+        <Box css={searchContainerStyle}>
+          <input
+            placeholder="Search Files"
+            onChange={handleInputChange}
+            css={textInputStyle}
+          />
 
-              <Icon
-                name="magnifier"
-                size="1.25rem"
-                style={{ padding: '0.125em', cursor: 'pointer' }}
-              />
-            </Box>
-          </Box>
-
-          {isFilesFetched && (
-            <Box
-              style={{
-                flex: '1',
-                overflow: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent:
-                  filteredFiles.length === 0 ? 'center' : 'initial',
-                alignItems: filteredFiles.length === 0 ? 'center' : 'initial',
-              }}
-            >
-              {filteredFiles.length === 0 ? (
-                <Box
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    color: '#4a4a4a',
-                  }}
-                >
-                  <Icon
-                    name="magnifier"
-                    size="3rem"
-                    style={{ padding: '0.5rem' }}
-                  />
-                  <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
-                    No files found
-                  </span>
-                </Box>
-              ) : (
-                filteredFiles.map(
-                  (file) =>
-                    file.path && (
-                      <Box key={file._id} css={MessageCss}>
-                        <FilePreviewContainer
-                          file={file}
-                          sequential={false}
-                          isStarred={false}
-                        />
-                        <MessageBodyContainer style={{ width: '75%' }}>
-                          <FilePreviewHeader
-                            file={file}
-                            isTimeStamped={false}
-                          />
-                          <MessageBody>
-                            <div css={FilePreviewUsernameCss}>
-                              @{file.user.username}
-                            </div>
-                          </MessageBody>
-                          <FileMetrics file={file} />
-                        </MessageBodyContainer>
-
-                        <Menu
-                          isToolTip={false}
-                          options={[
-                            {
-                              id: 'download',
-                              action: () => downloadFile(file.url, file.title),
-                              label: 'Download',
-                              icon: 'circle-arrow-down',
-                            },
-                            {
-                              id: 'delete',
-                              action: () => setFileToDelete(file),
-                              label: 'Delete',
-                              icon: 'trash',
-                            },
-                          ]}
-                        />
-                      </Box>
-                    )
-                )
-              )}
-            </Box>
-          )}
+          <Icon
+            name="magnifier"
+            size="1.25rem"
+            style={{ padding: '0.125em', cursor: 'pointer' }}
+          />
         </Box>
-      </Box>
+
+        {isFilesFetched && (
+          <Box
+            style={{
+              flex: '1',
+              overflow: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: filteredFiles.length === 0 ? 'center' : 'initial',
+              alignItems: filteredFiles.length === 0 ? 'center' : 'initial',
+            }}
+          >
+            {filteredFiles.length === 0 ? (
+              <Box
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  color: '#4a4a4a',
+                }}
+              >
+                <Icon
+                  name="magnifier"
+                  size="3rem"
+                  style={{ padding: '0.5rem' }}
+                />
+                <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                  No files found
+                </span>
+              </Box>
+            ) : (
+              filteredFiles.map(
+                (file) =>
+                  file.path && (
+                    <Box key={file._id} css={MessageCss}>
+                      <FilePreviewContainer
+                        file={file}
+                        sequential={false}
+                        isStarred={false}
+                      />
+                      <MessageBodyContainer style={{ width: '75%' }}>
+                        <FilePreviewHeader file={file} isTimeStamped={false} />
+                        <MessageBody>
+                          <div css={FilePreviewUsernameCss}>
+                            @{file.user.username}
+                          </div>
+                        </MessageBody>
+                        <FileMetrics file={file} />
+                      </MessageBodyContainer>
+
+                      <Menu
+                        isToolTip={false}
+                        options={[
+                          {
+                            id: 'download',
+                            action: () => downloadFile(file.url, file.title),
+                            label: 'Download',
+                            icon: 'circle-arrow-down',
+                          },
+                          {
+                            id: 'delete',
+                            action: () => setFileToDelete(file),
+                            label: 'Delete',
+                            icon: 'trash',
+                          },
+                        ]}
+                      />
+                    </Box>
+                  )
+              )
+            )}
+          </Box>
+        )}
+      </Sidebar>
     </>
   );
 };
--- packages/react/src/components/StarredMessages/StarredMessages.js
+++ packages/react/src/components/StarredMessages/StarredMessages.js
@@ -1,15 +1,14 @@
-import React, { useState, useContext, useMemo, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { isSameDay, format } from 'date-fns';
 import RCContext from '../../context/RCInstance';
-import classes from './StarredMessages.module.css';
 import { useStarredMessageStore } from '../../store';
 import { Box } from '../Box';
 import { Icon } from '../Icon';
-import { ActionButton } from '../ActionButton';
 import { MessageDivider } from '../Message/MessageDivider';
 import { Message } from '../Message';
 import isMessageSequential from '../../lib/isMessageSequential';
 import { Throbber } from '../Throbber';
+import Sidebar from '../Sidebar/Sidebar';
 
 const StarredMessages = () => {
   const { RCInstance } = useContext(RCContext);
@@ -17,115 +16,91 @@ const StarredMessages = () => {
     (state) => state.setShowStarred
   );
 
-  const [messageList, setMessageList] = useState([]);
+  const [messageList, setmessageList] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const toggleShowStarred = () => {
-    setShowStarred(false);
-  };
-
-  const getStarredMessages = async () => {
-    const { messages } = await RCInstance.getStarredMessages();
-    setMessageList(messages);
-    setLoading(false);
-  };
-
   useEffect(() => {
-    if (messageList.length === 0) {
-      getStarredMessages();
-    }
-  }, [messageList, getStarredMessages]);
+    const getStarredMessages = async () => {
+      const { messages } = await RCInstance.getStarredMessages();
+      setmessageList(messages);
+      setLoading(false);
+    };
+    getStarredMessages();
+  });
 
   const isMessageNewDay = (current, previous) =>
     !previous || !isSameDay(new Date(current.ts), new Date(previous.ts));
 
   return (
-    <Box className={classes.sidebar} style={{ padding: '1rem' }}>
-      <Box className={classes.wrapContainer}>
-        <Box
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: '0.5rem',
-            marginBottom: '1rem',
-          }}
-        >
-          <h3 style={{ display: 'contents' }}>
-            <Icon name="star" size="1.25rem" />
-            <Box style={{ color: '#4a4a4a', width: '80%' }}>
-              Starred Messages
-            </Box>
-            <ActionButton onClick={toggleShowStarred} ghost size="small">
-              <Icon name="cross" size="x20" />
-            </ActionButton>
-          </h3>
-        </Box>
-        <Box
-          style={{
-            flex: '1',
-            overflow: 'auto',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: messageList.length === 0 ? 'center' : 'initial',
-            alignItems: messageList.length === 0 ? 'center' : 'initial',
-          }}
-        >
-          {loading ? (
-            <Box
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                color: '#4a4a4a',
-              }}
-            >
-              <Throbber />
-            </Box>
-          ) : messageList.length === 0 ? (
-            <Box
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                color: '#4a4a4a',
-              }}
-            >
-              <Icon name="star" size="3rem" style={{ padding: '0.5rem' }} />
-              <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
-                No starred messages
-              </span>
-            </Box>
-          ) : (
-            messageList.map((msg, index, arr) => {
-              const prev = arr[index + 1];
-              const newDay = isMessageNewDay(msg, prev);
-              const sequential = isMessageSequential(msg, prev, 300);
-              return (
-                <Box key={msg._id}>
-                  {newDay && (
-                    <Box style={{ paddingTop: '0.5rem' }}>
-                      <MessageDivider>
-                        {format(new Date(msg.ts), 'MMMM d, yyyy')}
-                      </MessageDivider>
-                    </Box>
-                  )}
-                  <Message
-                    key={msg._id}
-                    message={msg}
-                    newDay={false}
-                    sequential={sequential}
-                    variant="default"
-                    showAvatar
-                    showToolbox={false}
-                  />
-                </Box>
-              );
-            })
-          )}
-        </Box>
+    <Sidebar
+      title="Starred Messages"
+      iconName="star"
+      setShowWindow={setShowStarred}
+    >
+      <Box
+        style={{
+          flex: '1',
+          overflow: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: messageList?.length === 0 ? 'center' : 'initial',
+          alignItems: messageList?.length === 0 ? 'center' : 'initial',
+        }}
+      >
+        {loading ? (
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              color: '#4a4a4a',
+            }}
+          >
+            <Throbber />
+          </Box>
+        ) : messageList?.length === 0 ? (
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              color: '#4a4a4a',
+            }}
+          >
+            <Icon name="star" size="3rem" style={{ padding: '0.5rem' }} />
+            <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+              No starred messages
+            </span>
+          </Box>
+        ) : (
+          messageList?.map((msg, index, arr) => {
+            const prev = arr[index + 1];
+            const newDay = isMessageNewDay(msg, prev);
+            const sequential = isMessageSequential(msg, prev, 300);
+            return (
+              <Box key={msg._id}>
+                {newDay && (
+                  <Box style={{ paddingTop: '0.5rem' }}>
+                    <MessageDivider>
+                      {format(new Date(msg.ts), 'MMMM d, yyyy')}
+                    </MessageDivider>
+                  </Box>
+                )}
+                <Message
+                  key={msg._id}
+                  message={msg}
+                  newDay={false}
+                  sequential={sequential}
+                  variant="default"
+                  showAvatar
+                  showToolbox={false}
+                />
+              </Box>
+            );
+          })
+        )}
       </Box>
-    </Box>
+    </Sidebar>
   );
 };
 export default StarredMessages;
