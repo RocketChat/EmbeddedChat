bc0e4ee4f4a44cf1679dbc231623fd50caf47248
Zishan Ahmad <zishan.barun@gmail.com>
2024-03-22 17:22:43 +0530

    made throbber consistent with rc in starred msgs

--- packages/react/src/components/StarredMessages/StarredMessages.js
+++ packages/react/src/components/StarredMessages/StarredMessages.js
@@ -36,69 +36,73 @@ const StarredMessages = () => {
       iconName="star"
       setShowWindow={setShowStarred}
     >
-      <Box
-        style={{
-          flex: '1',
-          overflow: 'auto',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: messageList?.length === 0 ? 'center' : 'initial',
-          alignItems: messageList?.length === 0 ? 'center' : 'initial',
-          overflowX: 'hidden',
-          maxWidth: '100%',
-        }}
-      >
-        {loading ? (
-          <Box
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              color: '#4a4a4a',
-            }}
-          >
-            <Throbber />
-          </Box>
-        ) : messageList?.length === 0 ? (
-          <Box
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              color: '#4a4a4a',
-            }}
-          >
-            <Icon name="star" size="3rem" style={{ padding: '0.5rem' }} />
-            <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
-              No starred messages
-            </span>
-          </Box>
-        ) : (
-          messageList?.map((msg, index, arr) => {
-            const newDay = index === 0 || isMessageNewDay(msg, arr[index - 1]);
-            return (
-              <Box key={msg._id}>
-                {newDay && (
-                  <Box style={{ paddingTop: '0.5rem' }}>
-                    <MessageDivider>
-                      {format(new Date(msg.ts), 'MMMM d, yyyy')}
-                    </MessageDivider>
-                  </Box>
-                )}
-                <Message
-                  key={msg._id}
-                  message={msg}
-                  newDay={false}
-                  variant="default"
-                  showAvatar
-                  showToolbox={false}
-                />
-              </Box>
-            );
-          })
-        )}
-      </Box>
+      {loading ? (
+        <Box
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            color: '#4a4a4a',
+          }}
+        >
+          <Throbber />
+        </Box>
+      ) : (
+        <Box
+          style={{
+            flex: '1',
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: messageList?.length === 0 ? 'center' : 'initial',
+            alignItems: messageList?.length === 0 ? 'center' : 'initial',
+            overflowX: 'hidden',
+            maxWidth: '100%',
+          }}
+        >
+          {messageList?.length === 0 ? (
+            <Box
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                color: '#4a4a4a',
+              }}
+            >
+              <Icon name="star" size="3rem" style={{ padding: '0.5rem' }} />
+              <span style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                No starred messages
+              </span>
+            </Box>
+          ) : (
+            messageList?.map((msg, index, arr) => {
+              const newDay =
+                index === 0 || isMessageNewDay(msg, arr[index - 1]);
+              return (
+                <Box key={msg._id}>
+                  {newDay && (
+                    <Box style={{ paddingTop: '0.5rem' }}>
+                      <MessageDivider>
+                        {format(new Date(msg.ts), 'MMMM d, yyyy')}
+                      </MessageDivider>
+                    </Box>
+                  )}
+                  <Message
+                    key={msg._id}
+                    message={msg}
+                    newDay={false}
+                    variant="default"
+                    showAvatar
+                    showToolbox={false}
+                  />
+                </Box>
+              );
+            })
+          )}
+        </Box>
+      )}
     </Sidebar>
   );
 };
+
 export default StarredMessages;
