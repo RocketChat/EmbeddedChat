eecb9678d5ba65993725d2af5b0947149a8f38f7
Zishan Ahmad <zishan.barun@gmail.com>
2024-04-06 22:45:21 +0530

    added fallback when imagefetch fails

--- packages/react/src/components/Attachments/ImageGallery.js
+++ packages/react/src/components/Attachments/ImageGallery.js
@@ -6,23 +6,30 @@ import classes from './ImageGallery.module.css';
 import { Throbber } from '../Throbber';
 import { ActionButton } from '../ActionButton';
 import { Icon } from '../Icon';
+import { Button } from '../Button';
 
 const ImageGallery = ({ currentFileId, setShowGallery }) => {
   const { RCInstance } = useRCContext();
   const [files, setFiles] = useState([]);
   const [currentFileIndex, setCurrentFileIndex] = useState(-1);
   const [loading, setLoading] = useState(true);
+  const [imgFetchErr, setImgFetchErr] = useState(false);
 
   useEffect(() => {
     const fetchAllImages = async () => {
       const res = await RCInstance.getAllImages();
-      if (res?.files) {
-        setFiles(res.files);
-        const fileIndex = res.files.findIndex(
-          (file) => file._id === currentFileId
-        );
-        setCurrentFileIndex(fileIndex);
+      if (res) {
+        if (res?.files) {
+          setFiles(res.files);
+          const fileIndex = res.files.findIndex(
+            (file) => file._id === currentFileId
+          );
+          setCurrentFileIndex(fileIndex);
+          setLoading(false);
+        }
+      } else {
         setLoading(false);
+        setImgFetchErr(true);
       }
     };
     fetchAllImages();
@@ -38,9 +45,41 @@ const ImageGallery = ({ currentFileId, setShowGallery }) => {
       >
         <Icon name="cross" />
       </ActionButton>
-      {loading ? (
+      {loading && (
         <Box className={classes.throbberWrapper}>
           <Throbber />
+        </Box>
+      )}
+
+      {imgFetchErr ? (
+        <Box
+          className={classes.fetchErrorWrapper}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Icon
+            name="magnifier"
+            size="3rem"
+            style={{ padding: '0.5rem', color: '#FF99A2' }}
+          />
+          <span
+            style={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#fff' }}
+          >
+            Something went wrong
+          </span>
+          <Button
+            color="primary"
+            onClick={() => setShowGallery(false)}
+            style={{
+              alignSelf: 'auto',
+              margin: '10px',
+            }}
+          >
+            Close
+          </Button>
         </Box>
       ) : (
         <Swiper
