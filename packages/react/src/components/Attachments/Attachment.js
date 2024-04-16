1b7c42cbab31001765b47e47d6eee5a6e980e95d
Akshun-01 <akshunkuthiala2002@gmail.com>
2024-04-16 14:52:21 +0530

    add support for quoting different types of attachments

--- packages/react/src/components/Attachments/Attachment.js
+++ packages/react/src/components/Attachments/Attachment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import ImageAttachment from './ImageAttachment';
 import AudioAttachment from './AudioAttachment';
@@ -6,8 +6,21 @@ import VideoAttachment from './VideoAttachment';
 import PinnedAttachment from './PinnedAttachment';
 import { Box } from '../Box';
 import { Icon } from '../Icon';
+import { Avatar } from '../Avatar';
+import RCContext from '../../context/RCInstance';
+import { Button } from '../Button';
+import ImageGallery from './ImageGallery';
+import AttachmentMetadata from './AttachmentMetadata';
+import { LinkPreview } from '../LinkPreview';
 
 const Attachment = ({ attachment, host }) => {
+  const { RCInstance } = useContext(RCContext);
+  const getUserAvatarUrl = (authorIcon) => {
+    const host = RCInstance.getHost();
+    const URL = `${host}${authorIcon}`;
+    return URL;
+  };
+
   if (attachment && attachment.audio_url) {
     return <AudioAttachment attachment={attachment} host={host} />;
   }
@@ -20,13 +33,52 @@ const Attachment = ({ attachment, host }) => {
   if (attachment && attachment.text) {
     return <PinnedAttachment attachment={attachment} />;
   }
+  
+  if(attachment.attachments[0]){
+    if (attachment.attachments[0].audio_url) {
+      return <AudioAttachment attachment={attachment.attachments[0]} host={host} />;
+    }
+    if (attachment.attachments[0].video_url) {
+      return  <VideoAttachment attachment={attachment.attachments[0]} host={host} />;
+    }
+    if (attachment.attachments[0].image_url) {
+      return <ImageAttachment attachment={attachment.attachments[0]} host={host} />;
+    }      
+  }
+
   return (
     <Box>
       <p>{attachment?.description}</p>
-      <p>
-        <Icon name="file" size="20px" />
-        <a href={`${host}${attachment.title_link}`}>{attachment.title}</a>
-      </p>
+      <Box
+        style={{
+          borderInlineStart: '1px solid currentColor',
+          paddingLeft: '0.8rem',
+        }}
+      >
+        <Box
+          style={{
+            display: 'flex',
+            gap: '0.3rem',
+          }}
+        >
+          <Avatar
+            url={getUserAvatarUrl(attachment?.author_icon)}
+            alt="avatar"
+            size="1.2em"
+          />
+          <Box>{attachment?.author_name}</Box>
+        </Box>
+        <Box
+          style={{
+            marginTop: '0.7rem',
+          }}
+        >
+          <Box>
+            <Icon name="file" size="20px" />
+            <a href={`${host}${attachment.title_link}`}>{attachment.title}</a>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
