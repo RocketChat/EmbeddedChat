1d7ddd2e5c4745427e266b7e4d4fd8b1962e275b
Zishan Ahmad <zishan.barun@gmail.com>
2024-05-09 20:47:03 +0530

    added divider.styles.js

---
+++ packages/react/src/components/Divider/Divider.styles.js
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+const styles = {
+  divider: css`
+    height: 2px;
+    margin: 0 8px 8px;
+    border: 0;
+    border-radius: 2px;
+    background-color: #e4e7ea;
+  `,
+};
+
+export default styles;
