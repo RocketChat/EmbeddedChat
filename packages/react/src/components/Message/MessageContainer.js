870e9bf944c24525b80cf3f91ce489f5992f928c
Zishan Ahmad <zishan.barun@gmail.com>
2024-05-11 00:09:38 +0530

    added msgstyles in message container

--- packages/react/src/components/Message/MessageContainer.js
+++ packages/react/src/components/Message/MessageContainer.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import useComponentOverrides from '../../theme/useComponentOverrides';
 import { Box } from '../Box';
 import { appendClassNames } from '../../lib/appendClassNames';
-
-const MessageContainerCss = css`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  width: 100%;
-  flex-grow: 1;
-  flex-shrink: 1;
-  min-width: 1px;
-  margin-top: -0.125rem;
-  margin-bottom: -0.125rem;
-  margin-block: -0.125rem;
-  margin-left: 0.25rem;
-  margin-right: 0.25rem;
-  margin-inline: 0.25rem;
-  &:hover {
-    background: #f2f3f5;
-  }
-`;
+import { MessageContainerStyles as styles } from './Message.styles';
 
 export const MessageContainer = ({
   children,
@@ -36,7 +17,7 @@ export const MessageContainer = ({
   );
   return (
     <Box
-      css={MessageContainerCss}
+      css={styles.messageContainer}
       className={appendClassNames('ec-message-container', classNames)}
       style={styleOverrides}
       {...props}
