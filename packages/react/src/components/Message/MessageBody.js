eb828b418b05e0c23f1dca43d7b75f0b77cf7e87
Zishan Ahmad <zishan.barun@gmail.com>
2024-05-11 00:03:11 +0530

    used msgstyles in messagebody

--- packages/react/src/components/Message/MessageBody.js
+++ packages/react/src/components/Message/MessageBody.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import useComponentOverrides from '../../theme/useComponentOverrides';
 import { Box } from '../Box';
 import { appendClassNames } from '../../lib/appendClassNames';
-
-const MessageBodyCss = css`
-  letter-spacing: 0rem;
-  font-size: 0.875rem;
-  font-weight: 400;
-  line-height: 1.25rem;
-  flex-shrink: 1;
-  transition: opacity 0.3s linear;
-  word-break: break-word;
-  opacity: 1;
-  color: #2f343d;
-  margin-top: 0.125rem;
-  margin-bottom: 0.125rem;
-  margin-block: 0.125rem;
-`;
+import { MessageBodyStyles as styles } from './Message.styles';
 
 export const MessageBody = ({
   children,
@@ -32,7 +17,7 @@ export const MessageBody = ({
   );
   return (
     <Box
-      css={MessageBodyCss}
+      css={styles.messageBody}
       className={appendClassNames('ec-message-body', classNames)}
       style={styleOverrides}
       {...props}
