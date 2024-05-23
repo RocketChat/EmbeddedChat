f476596fe46846561bb6247a46dc0e044e70597b
Zishan Ahmad <zishan.barun@gmail.com>
2024-05-23 10:48:14 +0530

    fixed modal stories

--- packages/react/src/lib/reactPortal.js
+++ packages/react/src/lib/reactPortal.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-function ReactPortal({ children, wrapperId }) {
-  const portalContainer = document.getElementById(wrapperId);
+function createWrapperAndAppendToBody(wrapperId) {
+  const wrapperElement = document.createElement('div');
+  wrapperElement.setAttribute('id', wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+}
+
+function ReactPortal({ children, wrapperId = 'react-portal-wrapper' }) {
+  const [portalContainer, setPortalContainer] = useState(null);
+
+  useEffect(() => {
+    let element = document.getElementById(wrapperId);
+    let systemCreated = false;
+
+    if (!element) {
+      systemCreated = true;
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+
+    setPortalContainer(element);
+
+    return () => {
+      if (systemCreated && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    };
+  }, [wrapperId]);
 
   if (!portalContainer) {
     return null;
