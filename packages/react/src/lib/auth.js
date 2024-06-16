ae64468f201945b2797b1c541f1c7993304d808f
Zishan Ahmad <zishan.barun@gmail.com>
2024-06-16 16:56:37 +0530

    testing endpoints

--- packages/react/src/lib/auth.js
+++ packages/react/src/lib/auth.js
@@ -1,14 +1,49 @@
 export async function saveToken(token) {
-  if (typeof localStorage !== 'undefined') {
-    localStorage.setItem('ec_token', token);
-  }
+  const urlencoded = new URLSearchParams();
+  urlencoded.append('token', token);
+
+  const requestOptions = {
+    method: 'POST',
+    body: urlencoded,
+    redirect: 'follow',
+    credentials: 'include',
+  };
+
+  fetch(
+    'http://localhost:3000/api/apps/public/4c977b2e-eda2-4627-8bfe-2d0358304a79/save-token',
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 }
 
 export async function getToken() {
-  if (typeof localStorage !== 'undefined') {
-    return localStorage.getItem('ec_token');
+  const requestOptions = {
+    method: 'GET',
+    credentials: 'include',
+    redirect: 'follow',
+  };
+
+  try {
+    const response = await fetch(
+      'http://localhost:3000/api/apps/public/4c977b2e-eda2-4627-8bfe-2d0358304a79/get-token',
+      requestOptions
+    );
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const result = await response.json();
+
+    const { token } = result;
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.error('Error fetching token:', error);
+    return null;
   }
-  return null;
 }
 
 export async function deleteToken() {
