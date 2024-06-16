a9045133ab4918559c9d824fdb3d45016419f91c
Zishan Ahmad <zishan.barun@gmail.com>
2024-06-16 21:55:37 +0530

    removed test codes

--- packages/react/src/lib/auth.js
+++ packages/react/src/lib/auth.js
@@ -1,63 +1,18 @@
 export async function saveToken(token) {
-  const urlencoded = new URLSearchParams();
-  urlencoded.append('token', token);
-
-  const requestOptions = {
-    method: 'POST',
-    body: urlencoded,
-    redirect: 'follow',
-    credentials: 'include',
-  };
-
-  fetch(
-    'http://localhost:3000/api/apps/public/4c977b2e-eda2-4627-8bfe-2d0358304a79/auth-token',
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
-}
-
-export async function getToken() {
-  const requestOptions = {
-    method: 'GET',
-    credentials: 'include',
-    redirect: 'follow',
-  };
-
-  try {
-    const response = await fetch(
-      'http://localhost:3000/api/apps/public/4c977b2e-eda2-4627-8bfe-2d0358304a79/auth-token',
-      requestOptions
-    );
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const result = await response.json();
-
-    const { token } = result;
-    console.log(token);
-    return token;
-  } catch (error) {
-    console.error('Error fetching token:', error);
-    return null;
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('ec_token', token);
   }
 }
 
-export async function deleteToken() {
-  const requestOptions = {
-    method: 'DELETE',
-    redirect: 'follow',
-    credentials: 'include',
-  };
+export async function getToken() {
+  if (typeof localStorage !== 'undefined') {
+    return localStorage.getItem('ec_token');
+  }
+  return null;
+}
 
-  fetch(
-    'http://localhost:3000/api/apps/public/4c977b2e-eda2-4627-8bfe-2d0358304a79/auth-token',
-    requestOptions
-  )
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+export async function deleteToken() {
+  if (typeof localStorage !== 'undefined') {
+    localStorage.removeItem('ec_token');
+  }
 }
