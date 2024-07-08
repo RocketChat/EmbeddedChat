8436ef2e3bca687c69fde40375978a6b9817ed68
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-08 18:03:29 +0530

    changed way of export

--- packages/ui-elements/src/index.js
+++ packages/ui-elements/src/index.js
@@ -1,34 +1,4 @@
-export { ActionButton } from './components/ActionButton';
-export { Box } from './components/Box';
-export { CheckBox } from './components/CheckBox';
-export { Flex } from './components/Flex';
-export { Grid } from './components/Grid';
-export { Icon } from './components/Icon';
-export { ListBox } from './components/ListBox';
-export { Modal } from './components/Modal';
-export { Popup } from './components/Popup';
-export { Skeleton } from './components/Skeleton';
-export { Throbber } from './components/Throbber';
-export { Tooltip } from './components/Tooltip';
-export { Avatar } from './components/Avatar';
-export { Button } from './components/Button';
-export { Divider } from './components/Divider';
-export { GenericModal } from './components/GenericModal';
-export { Heading } from './components/Heading';
-export { Input } from './components/Input';
-export { Menu } from './components/Menu';
-export { MultiSelect } from './components/MultiSelect';
-export {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  MinimalSidebar,
-} from './components/Sidebar';
-export { StaticSelect } from './components/StaticSelect';
-export { ToastBar } from './components/ToastBar';
-export * from './components/MessageGenericPreview';
-export { ToastBarProvider } from './components/ToastBar';
+export * from './components';
 export { ThemeProvider } from './context/ThemeContextProvider';
 export * from './hooks';
 export * from './lib';
