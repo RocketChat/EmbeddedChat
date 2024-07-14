f529b9968cc25b165302975057a70e2f0d6e2e2c
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-14 23:13:20 +0530

    fixed mode

--- packages/layout_editor/src/views/LayoutEditor.jsx
+++ packages/layout_editor/src/views/LayoutEditor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DefaultTheme from "../theme/DefaultTheme";
 import { Box, ThemeProvider } from "@embeddedchat/ui-elements";
 import { styles } from "./LayoutEditor.style";
@@ -6,18 +6,21 @@ import ChatLayout from "./ChatLayout/ChatLayout";
 import ChatHeader from "./ChatHeader/ChatHeader";
 import GlobalStyles from "./GlobalStyles";
 
-const LayoutEditor = () => (
-  <ThemeProvider theme={DefaultTheme} mode="light">
-    <Box
-      css={styles.embeddedchat(DefaultTheme, false)}
-      className="ec-embedded-chat"
-    >
-      <GlobalStyles />
-      <ChatHeader />
-      <ChatLayout />
-      <div id="overlay-items" />
-    </Box>
-  </ThemeProvider>
-);
+const LayoutEditor = () => {
+  const [mode, setMode] = useState("light");
+  return (
+    <ThemeProvider theme={DefaultTheme} mode={mode}>
+      <Box
+        css={styles.embeddedchat(DefaultTheme, mode === "light" ? false : true)}
+        className="ec-embedded-chat"
+      >
+        <GlobalStyles />
+        <ChatHeader />
+        <ChatLayout />
+        <div id="overlay-items" />
+      </Box>
+    </ThemeProvider>
+  );
+};
 
 export default LayoutEditor;
