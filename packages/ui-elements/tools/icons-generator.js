53784f7640602b86504418cb26d9138261c0af22
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-14 14:03:46 +0530

    icon generator to not edit index file

--- packages/ui-elements/tools/icons-generator.js
+++ packages/ui-elements/tools/icons-generator.js
@@ -6,7 +6,6 @@ const iconsList = [
   'file',
   'mobile',
   'star',
-  'pin',
   'reply-directly',
   'hash',
   'computer',
@@ -42,7 +41,6 @@ const iconsList = [
   'chevron-down',
   'chevron-left',
   'key',
-  'attachment',
   'quote',
   'at',
   'arrow-collapse',
@@ -80,24 +78,6 @@ const getComponentCode = (name, svgData) => `
     export default ${camelCase(name)};
   `;
 
-const getIndexFileCode = () => {
-  const importCodes = iconsList
-    .map((icon) => `import ${camelCase(icon)} from './${camelCase(icon)}';`)
-    .join('\n');
-  const exportCodes = iconsList
-    .map((icon) => `"${icon}": ${camelCase(icon)}`)
-    .join(',\n');
-  return `
-    ${importCodes}
-
-    const icons = {
-      ${exportCodes}
-    };
-
-    export default icons;
-  `;
-};
-
 if (!fs.existsSync(svgIconOutputDir)) {
   fs.mkdirSync(svgIconOutputDir, { recursive: true });
 }
@@ -112,5 +92,4 @@ iconsList.forEach((icon) => {
   fs.writeFileSync(svgIconOutputPath, componentCode);
 });
 
-fs.writeFileSync(path.join(svgIconOutputDir, 'index.js'), getIndexFileCode());
 execSync(`npx prettier --write '${svgIconOutputDir}' `);
