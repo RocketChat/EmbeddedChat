a7dfc108da32d99ce0838c47054680187d52e1cf
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-18 00:06:34 +0530

    added layout and palette section:

--- packages/layout_editor/src/views/ThemeLab/ThemeLab.jsx
+++ packages/layout_editor/src/views/ThemeLab/ThemeLab.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Box, MinimalSidebar, SidebarHeader } from '@embeddedchat/ui-elements';
+import React, { useState } from 'react';
+import {
+  Box,
+  MinimalSidebar,
+  SidebarContent,
+  SidebarHeader,
+  useTheme,
+} from '@embeddedchat/ui-elements';
 import useLayoutStore from '../../store/layoutStore';
 import { useThemeLabStyles } from './ThemeLab.styles';
 
 const ThemeLab = () => {
-  const styles = useThemeLabStyles();
+  const styles = useThemeLabStyles(useTheme());
   const setThemeLabOpen = useLayoutStore((state) => state.setThemeLabOpen);
+  const [paletteActive, setPaletteAction] = useState(true);
+
   return (
     <Box>
       <MinimalSidebar>
@@ -14,6 +22,24 @@ const ThemeLab = () => {
           title="Theme Lab"
           iconName="cog"
         />
+        <SidebarContent>
+          <Box css={styles.sectionContainer}>
+            <Box
+              is="span"
+              css={[styles.section, paletteActive && styles.sectionActive]}
+              onClick={() => setPaletteAction(true)}
+            >
+              Palette
+            </Box>
+            <Box
+              is="span"
+              css={[styles.section, !paletteActive && styles.sectionActive]}
+              onClick={() => setPaletteAction(false)}
+            >
+              Layout
+            </Box>
+          </Box>
+        </SidebarContent>
       </MinimalSidebar>
     </Box>
   );
--- packages/layout_editor/src/views/ThemeLab/ThemeLab.styles.js
+++ packages/layout_editor/src/views/ThemeLab/ThemeLab.styles.js
@@ -1,9 +1,19 @@
 import { css } from '@emotion/react';
 
-export const useThemeLabStyles = () => {
+export const useThemeLabStyles = ({ colors }) => {
   const styles = {
-    themeLab: css`
+    sectionContainer: css`
       display: flex;
+      align-items: center;
+      justify-content: space-around;
+    `,
+
+    section: css`
+      padding: 0 1rem 0.5rem;
+      cursor: pointer;
+    `,
+    sectionActive: css`
+      border-bottom: 1px solid ${colors.primary};
     `,
   };
 
