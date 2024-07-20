9a49d4edeec31774be076748564dfc72d4ac3ffe
Zishan Ahmad <zishan.barun@gmail.com>
2024-07-20 14:49:16 +0530

    added default heading styles when theme is not present

--- packages/ui-elements/src/components/Heading/Heading.js
+++ packages/ui-elements/src/components/Heading/Heading.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import useTheme from '../../hooks/useTheme';
+import { useTheme } from '../../hooks';
 
 const Heading = ({ level = 1, children, ...props }) => {
-  const { theme } = useTheme();
+  const defaultTypography = {
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 800,
+      lineHeight: 1.5,
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 800,
+      lineHeight: 1.4,
+    },
+    h3: {
+      fontSize: '1.3rem',
+      fontWeight: 400,
+      lineHeight: 1.3,
+    },
+    h4: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.2,
+    },
+    h5: {
+      fontSize: '0.83rem',
+      fontWeight: 400,
+      lineHeight: 1.1,
+    },
+    h6: {
+      fontSize: '0.67rem',
+      fontWeight: 500,
+      lineHeight: 1.0,
+    },
+  };
+
   const Tag = `h${level}`;
-  const style = theme.typography[Tag];
+  const { theme } = useTheme();
+  const style = { ...defaultTypography[Tag], ...theme.typography[Tag] };
+
   return (
     <Tag
       css={css`
