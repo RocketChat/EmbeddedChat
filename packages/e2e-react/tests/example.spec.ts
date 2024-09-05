652e6902235c9f512430a455db6b3260822300ca
Zishan Ahmad <zishan.barun@gmail.com>
2024-09-05 11:25:24 +0530

    removed playwright message test as abhinav server is no longer working, and demo.qa doesn't support anonymousMode

--- packages/e2e-react/tests/example.spec.ts
+++ packages/e2e-react/tests/example.spec.ts
@@ -1,18 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('EmbeddedChat should render', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('.ec-embedded-chat')).toBeVisible();
+test("EmbeddedChat should render", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator(".ec-embedded-chat")).toBeVisible();
 });
 
-test('EmbeddedChat has a title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('.ec-chat-header--channelName')).toHaveText('Login to chat');
-});
-
-test('EmbeddedChat has messages', async ({ page }) => {
-  await page.goto('/');
-
-  await page.waitForSelector('.ec-message');
-  expect(await page.locator('.ec-message').count()).toBeGreaterThan(0);
+test("EmbeddedChat has a title", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator(".ec-chat-header--channelName")).toHaveText(
+    "Login to chat"
+  );
 });
