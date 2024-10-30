583d6b6448d2f7796e05f8a93b156c886b2912e6
Anirban Singha <anirbansingha20@gmail.com>
2024-10-31 00:57:08 +0530

    fix: resolve mention highlighting issue in File Upload descriptions

--- packages/react/src/views/AttachmentHandler/Attachment.js
+++ packages/react/src/views/AttachmentHandler/Attachment.js
@@ -6,8 +6,12 @@ import ImageAttachment from './ImageAttachment';
 import AudioAttachment from './AudioAttachment';
 import VideoAttachment from './VideoAttachment';
 import TextAttachment from './TextAttachment';
+import { useUserStore } from '../../store';
 
 const Attachment = ({ attachment, host, type, variantStyles = {} }) => {
+  const { username } = useUserStore((state) => ({
+    username: state.username,
+  }));
   if (attachment && attachment.audio_url) {
     return (
       <AudioAttachment
@@ -44,14 +48,54 @@ const Attachment = ({ attachment, host, type, variantStyles = {} }) => {
       />
     );
   }
+  const parseMentions = (msg) => {
+    const mentionRegex = /(@\w+)/g;
+    const parts = msg.split(mentionRegex);
+    return parts;
+  };
+  const parts = parseMentions(attachment.description);
+
   return (
     <Box
       css={css`
         display: flex;
       `}
     >
-      {attachment?.description}
-
+      <div>
+        {parts.map((part, index) =>
+          part.startsWith('@') ? (
+            part.slice(1) !== username ? (
+              <span
+                key={index}
+                css={css`
+                  color: #71717a;
+                  font-weight: bold;
+                  background-color: #f4f4f5;
+                  padding: 1.5px;
+                  border-radius: 3px;
+                `}
+              >
+                {part.slice(1)}
+              </span>
+            ) : (
+              <span
+                key={index}
+                css={css`
+                  color: #fafafa;
+                  font-weight: bold;
+                  background-color: #ef4444;
+                  padding: 1.5px;
+                  border-radius: 3px;
+                `}
+              >
+                {part.slice(1)}
+              </span>
+            )
+          ) : (
+            <span key={index}>{part}</span>
+          )
+        )}
+      </div>
       <Icon name="file" size="20px" />
       <a href={`${host}${attachment.title_link}`}>{attachment.title}</a>
     </Box>
