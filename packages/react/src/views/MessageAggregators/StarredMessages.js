12c9608b8f94288e5d80c50368679945a0ce03bf
Rahul Singh Thakur <65606499+Barrylimarti@users.noreply.github.com>
2024-11-02 21:44:15 +0530

    [fix] Re-rendering fixed while opening pinned and starred messages. (#644)
    * added memoization to stop rerenders
    
    * added memoization for allmessage and shouldrender
    
    * fixed linting errors and imports

--- packages/react/src/views/MessageAggregators/StarredMessages.js
+++ packages/react/src/views/MessageAggregators/StarredMessages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useComponentOverrides } from '@embeddedchat/ui-elements';
 import { useUserStore } from '../../store';
 import { MessageAggregator } from './common/MessageAggregator';
@@ -7,15 +7,18 @@ const StarredMessages = () => {
   const authenticatedUserId = useUserStore((state) => state.userId);
   const { variantOverrides } = useComponentOverrides('StarredMessages');
   const viewType = variantOverrides.viewType || 'Sidebar';
+  const shouldRender = useCallback(
+    (msg) =>
+      msg.starred &&
+      msg.starred.some((star) => star._id === authenticatedUserId),
+    [authenticatedUserId]
+  );
   return (
     <MessageAggregator
       title="Starred Messages"
       iconName="star"
       noMessageInfo="No Starred Messages"
-      shouldRender={(msg) =>
-        msg.starred &&
-        msg.starred.some((star) => star._id === authenticatedUserId)
-      }
+      shouldRender={shouldRender}
       viewType={viewType}
     />
   );
