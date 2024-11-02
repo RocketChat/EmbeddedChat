9099358dbeac077bf282c4f4d1b2612c4f325c5b
Smriti Doneria <smritidoneria@gmail.com>
2024-11-02 21:49:44 +0530

    fixed  markdown rendering in report message (#611)
    * fix
    
    * formatting fix
    
    * fix
    
    * Fiexed lint issues

--- packages/react/src/views/ReportMessage/MessageReportWindow.js
+++ packages/react/src/views/ReportMessage/MessageReportWindow.js
@@ -2,17 +2,10 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box, Input } from '@embeddedchat/ui-elements';
 import ReportWindowButtons from './ReportWindowButtons';
-import { useMessageStore } from '../../store';
 import styles from './ReportMessage.styles';
 
 const MessageReportWindow = ({ messageId }) => {
   const [reportDescription, setDescription] = useState('');
-  const messages = useMessageStore((state) => state.messages) || [];
-  const threadMessages = useMessageStore((state) => state.threadMessages) || [];
-  const allMessages = [...messages, ...threadMessages];
-  const messageText = allMessages.filter(
-    (message) => message._id === messageId
-  )[0]?.msg;
   return (
     <ReportWindowButtons
       variant="danger"
@@ -22,7 +15,6 @@ const MessageReportWindow = ({ messageId }) => {
       reportDescription={reportDescription}
       messageId={messageId}
     >
-      <Box>{JSON.stringify(messageText)}</Box>
       <Box css={styles.conatiner}>
         <Input
           textArea
