346176a7a0365d81608420ad6a02f448b0d3da62
abirc8010 <abirc8010@gmail.com>
2024-12-25 23:11:54 +0530

    Added room announcement feature , display avatar and make room name clickable

--- packages/react/src/views/RoomInformation/RoomInformation.js
+++ packages/react/src/views/RoomInformation/RoomInformation.js
@@ -9,11 +9,12 @@ import {
 } from '@embeddedchat/ui-elements';
 import RCContext from '../../context/RCInstance';
 import { useChannelStore } from '../../store';
+import getRoomInformationStyles from './RoomInformation.styles';
 import useSetExclusiveState from '../../hooks/useSetExclusiveState';
 
 const Roominfo = () => {
   const { RCInstance } = useContext(RCContext);
-
+  const styles = getRoomInformationStyles();
   const channelInfo = useChannelStore((state) => state.channelInfo);
   const { variantOverrides } = useComponentOverrides('RoomMember');
   const viewType = variantOverrides.viewType || 'Sidebar';
@@ -44,34 +45,35 @@ const Roominfo = () => {
           overflow: auto;
         `}
       >
-        <Avatar size="100%" url={getChannelAvatarURL(channelInfo.name)} />
         <Box
           css={css`
-            margin: 16px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
           `}
         >
-          <Box
-            css={css`
-              margin-block: 16px;
-              font-size: 1.25rem;
-            `}
-          >
-            # {channelInfo.name}
-          </Box>
-          <Box
-            css={css`
-              margin-block: 5px;
-            `}
-          >
-            Description
-          </Box>
-          <Box
-            css={css`
-              opacity: 0.5rem;
-            `}
-          >
-            {channelInfo.description}
-          </Box>
+          <Avatar size="100%" url={getChannelAvatarURL(channelInfo.name)} />
+        </Box>
+        <Box css={styles.infoContainer}>
+          <Box css={styles.infoHeader}># {channelInfo.name}</Box>
+          {channelInfo.description && (
+            <>
+              <Box css={styles.infoHeader}>Description</Box>
+              <Box css={styles.info}>{channelInfo.description}</Box>
+            </>
+          )}
+          {channelInfo.description && (
+            <>
+              <Box css={styles.infoHeader}>Topic</Box>
+              <Box css={styles.info}>{channelInfo.topic}</Box>
+            </>
+          )}
+          {channelInfo.description && (
+            <>
+              <Box css={styles.infoHeader}>Announcement</Box>
+              <Box css={styles.info}>{channelInfo.announcement}</Box>
+            </>
+          )}
         </Box>
       </Box>
     </ViewComponent>
