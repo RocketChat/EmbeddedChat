6223cbf61bb17577132d781aa48c10a546510b24
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2024-12-26 19:32:15 +0530

    lint

--- packages/react/src/views/SurfaceMenu/ToolbarMenu.js
+++ packages/react/src/views/SurfaceMenu/ToolbarMenu.js
@@ -5,25 +5,41 @@ import { Menu } from '@embeddedchat/ui-elements';
 const ToolbarMenu = ({ options, size = 'small' }) => (
   <>
     {options?.map((item, idx) => {
-        if (item.label !== 'Delete' && item.label !== 'Pin' && item.label !== 'Star' && item.label !== 'Report' && item.label !== 'Copy link' && item.label !== 'Copy message') {
-            return <SurfaceItem item={item} size={size} key={idx} />;
-        }
+      if (
+        item.label !== 'Delete' &&
+        item.label !== 'Pin' &&
+        item.label !== 'Star' &&
+        item.label !== 'Report' &&
+        item.label !== 'Copy link' &&
+        item.label !== 'Copy message'
+      ) {
+        return <SurfaceItem item={item} size={size} key={idx} />;
+      }
     })}
     <Menu
-        size='small'
-        tooltip={{ isToolTip: true, position: 'top', text: 'Options' }}
-        style={{ top: 'auto', bottom: `calc(100% + 2px)` }}
-        options={options.map((item, idx) => {
-            if (item.label === 'Delete' || item.label === 'Pin' || item.label === 'Star' || item.label === 'Report' || item.label === 'Copy link' || item.label === 'Copy message') {
-                return {
-                    id: item.id,
-                    action: item.onClick,
-                    label: item.label,
-                    icon: item.iconName,
-                };
-            }
-            return null;
-        }).filter(item => item !== null)}
+      size="small"
+      tooltip={{ isToolTip: true, position: 'top', text: 'Options' }}
+      style={{ top: 'auto', bottom: `calc(100% + 2px)` }}
+      options={options
+        .map((item, idx) => {
+          if (
+            item.label === 'Delete' ||
+            item.label === 'Pin' ||
+            item.label === 'Star' ||
+            item.label === 'Report' ||
+            item.label === 'Copy link' ||
+            item.label === 'Copy message'
+          ) {
+            return {
+              id: item.id,
+              action: item.onClick,
+              label: item.label,
+              icon: item.iconName,
+            };
+          }
+          return null;
+        })
+        .filter((item) => item !== null)}
     />
   </>
 );
