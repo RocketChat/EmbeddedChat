eb9451e16f82293156ba39a981e6e7396fb3a64d
Anirban Singha <anirbansingha20@gmail.com>
2024-12-28 16:15:44 +0530

    Simplify changes

--- packages/api/playground/playground.js
+++ packages/api/playground/playground.js
@@ -127,43 +127,10 @@ const toggle = (passwordField, togglePassword) => {
 
 	if(isPasswordVisible){
 		passwordField.type = "text"
-		togglePassword.innerHTML = `
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="20"
-				height="20"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.575 3.012 9.964 7.183a1.012 1.012 0 0 1 0 .639C20.575 16.488 16.638 19.5 12 19.5c-4.638 0-8.575-3.012-9.964-7.183ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
-				/>
-			</svg>
-		`;
+		togglePassword.innerText = "Hide";
 	} else {
 		passwordField.type = "password";	
-
-		togglePassword.innerHTML = `
-			<svg
-				height="20"
-				width="20"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-				/>
-			</svg>
-		`;
+		togglePassword.innerText = "Show";
 	}	
 }
 
