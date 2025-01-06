416228f4e21467ea1d443233c43fbab92f7110bb
abirc8010 <abirc8010@gmail.com>
2025-01-06 22:39:06 +0530

    Add tooltip to reactions

--- packages/react/src/views/Message/MessageReactions.js
+++ packages/react/src/views/Message/MessageReactions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   useComponentOverrides,
@@ -24,6 +24,33 @@ export const MessageReactions = ({
   );
   const { theme } = useTheme();
   const styles = getMessageReactionsStyles(theme);
+  const [hoveredReaction, setHoveredReaction] = useState(null);
+  const tooltipMap = {};
+  const getTooltipContent = (reaction) => {
+    const usernames = [...(reaction.usernames || [])];
+    const userIndex = usernames.indexOf(authenticatedUserUsername);
+    if (userIndex !== -1) {
+      usernames.splice(userIndex, 1);
+      usernames.unshift('You');
+    }
+    const visibleNames = usernames.slice(0, 9);
+    const remainingCount = usernames.length - visibleNames.length;
+    let tooltipContent = visibleNames.join(', ');
+    if (remainingCount > 0) {
+      tooltipContent += `, and ${remainingCount} ${
+        remainingCount === 1 ? 'other' : 'others'
+      }`;
+    }
+    tooltipMap[
+      reaction.name
+    ] = `${tooltipContent} reacted with ${reaction.name}`;
+    return tooltipMap[reaction.name];
+  };
+  message.reactions &&
+    serializeReactions(message.reactions).forEach((reaction) => {
+      getTooltipContent(reaction);
+    });
+
   return (
     <Box
       css={styles.container}
@@ -34,12 +61,12 @@ export const MessageReactions = ({
       {message.reactions &&
         serializeReactions(message.reactions).map((reaction) => (
           <Box
+            key={reaction.name}
             css={
               isSameUser(reaction, authenticatedUserUsername)
                 ? [styles.reaction, styles.reactionMine]
                 : [styles.reaction]
             }
-            key={reaction.name}
             mine={isSameUser(reaction, authenticatedUserUsername)}
             onClick={() =>
               handleEmojiClick(
@@ -48,9 +75,15 @@ export const MessageReactions = ({
                 !isSameUser(reaction, authenticatedUserUsername)
               )
             }
+            onMouseEnter={() => setHoveredReaction(reaction.name)}
+            onMouseLeave={() => setHoveredReaction(null)}
+            style={{ position: 'relative' }}
           >
-            <Markdown body={reaction.name} isReaction />
+            <Markdown body={reaction.name} isReaction titleTagVisible={false} />
             <p>{reaction.count}</p>
+            {hoveredReaction === reaction.name && (
+              <Box css={styles.emojiTooltip}>{tooltipMap[reaction.name]}</Box>
+            )}
           </Box>
         ))}
     </Box>
