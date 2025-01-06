cf9168a3c5cd170c33f83d2c555c4f2180323ea9
dhairyashil <dhairyashil10101010@gmail.com>
2025-01-06 10:37:00 +0530

    Changed logic to EventListener

--- packages/react/src/views/AttachmentPreview/AttachmentPreview.js
+++ packages/react/src/views/AttachmentPreview/AttachmentPreview.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import { css } from '@emotion/react';
 import { Box, Icon, Button, Input, Modal } from '@embeddedchat/ui-elements';
 import useAttachmentWindowStore from '../../store/attachmentwindow';
@@ -24,6 +24,7 @@ const AttachmentPreview = () => {
   const [filteredMembers, setFilteredMembers] = useState([]);
   const [mentionIndex, setMentionIndex] = useState(-1);
   const [startReadMentionUser, setStartReadMentionUser] = useState(false);
+  const [keyPressed, setKeyPressed] = useState(null);
 
   const [fileName, setFileName] = useState(data?.name);
 
@@ -66,20 +67,26 @@ const AttachmentPreview = () => {
     }
   };
 
-  const onKeyDown = (e) => {
-    switch (true) {
-      case e.code === 'Escape':
+  useEffect(() => {
+    const keyHandler = (e) => {
+      if (e.key === 'Enter') {
         e.preventDefault();
-        toggle();
-        break;
-      case e.code === 'Enter':
-        e.preventDefault();
-        submit();
-        break;
-      default:
-        break;
+        setKeyPressed('Enter');
+      }
+    };
+
+    document.addEventListener('keydown', keyHandler);
+    return () => {
+      document.removeEventListener('keydown', keyHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (keyPressed === 'Enter') {
+      submit();
+      setKeyPressed(null);
     }
-  };
+  }, [keyPressed, submit]);
 
   return (
     <Modal onClose={toggle}>
@@ -128,7 +135,6 @@ const AttachmentPreview = () => {
                 value={fileName}
                 css={styles.input}
                 placeholder="name"
-                onKeyDown={onKeyDown}
               />
               <TypingUsers />
             </Box>
@@ -167,7 +173,6 @@ const AttachmentPreview = () => {
                   css={styles.input}
                   placeholder="Description"
                   ref={messageRef}
-                  onKeyDown={onKeyDown}
                 />
               </Box>
             </Box>
