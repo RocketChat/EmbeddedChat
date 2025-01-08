59dc70160eb168d485647514395a79f004e87d1b
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2025-01-09 01:16:59 +0530

    add missing css for blockquote

--- packages/markups/src/blocks/QuoteBlock.js
+++ packages/markups/src/blocks/QuoteBlock.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ParagraphBlock from './ParagraphBlock';
+import { css } from '@emotion/react';
+import { useTheme } from '@embeddedchat/ui-elements';
 
-const QuoteBlock = ({ contents }) => (
-  <blockquote>
-    {contents.map((paragraph, index) => (
-      <ParagraphBlock key={index} contents={paragraph.value} />
-    ))}
-  </blockquote>
-);
+const QuoteBlock = ({ contents }) => {
+  const { theme } = useTheme();
+  return (
+    <blockquote
+      css={css`
+        background-color: ${theme.colors.secondary};
+        border-left: 1.7px solid ${theme.colors.primary};
+        padding-left: 0.5rem;
+        `}
+    >
+      {contents.map((paragraph, index) => (
+        <ParagraphBlock key={index} contents={paragraph.value} />
+      ))}
+    </blockquote>
+  );
+};
 
 export default QuoteBlock;
 
