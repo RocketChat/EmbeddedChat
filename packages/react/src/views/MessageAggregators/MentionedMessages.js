cc24196ba3b5d9030a959305e9f941043dde9397
Anirban Singha <anirbansingha20@gmail.com>
2025-01-11 18:13:20 +0530

    fix: ensure mentioned users in file description appear in mentions list

--- packages/react/src/views/MessageAggregators/MentionedMessages.js
+++ packages/react/src/views/MessageAggregators/MentionedMessages.js
@@ -7,14 +7,27 @@ const MentionedMessages = () => {
   const authenticatedUserId = useUserStore((state) => state.userId);
   const { variantOverrides } = useComponentOverrides('MentionedMessages');
   const viewType = variantOverrides.viewType || 'Sidebar';
+  const hasMention = (msg) => {
+    if (!msg.attachments?.length) {
+      return false;
+    }
+
+    return msg.attachments.some((attachment) =>
+      attachment.descriptionMd?.some((desc) =>
+        desc.value?.some((val) => val.type === 'MENTION_USER')
+      )
+    );
+  };
+
   return (
     <MessageAggregator
       title="Mentions"
       iconName="at"
       noMessageInfo="No mentions found"
       shouldRender={(msg) =>
-        msg.mentions &&
-        msg.mentions.some((star) => star._id === authenticatedUserId)
+        (msg.mentions &&
+          msg.mentions.some((star) => star._id === authenticatedUserId)) ||
+        hasMention(msg)
       }
       viewType={viewType}
     />
