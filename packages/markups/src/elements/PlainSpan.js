dec8e0a353595280c0e7eb0dc06027e6692c0ae2
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2025-01-11 16:16:02 +0530

    also take care of empty string after >

--- packages/markups/src/elements/PlainSpan.js
+++ packages/markups/src/elements/PlainSpan.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
+import { useTheme } from '@embeddedchat/ui-elements';
 
-const PlainSpan = ({ contents }) => <>{contents}</>;
-
+const PlainSpan = ({ contents }) => {
+  console.log(contents);
+  const { theme } = useTheme();
+  if (contents === '>') {
+    return (
+      <blockquote
+        css={css`
+          background-color: ${theme.colors.secondary};
+          border-left: 1.7px solid ${theme.colors.primary};
+          padding-left: 0.5rem;
+        `}
+      >
+        &nbsp;
+      </blockquote>
+    );
+  }
+  return <>{contents}</>;
+};
 export default PlainSpan;
 
 PlainSpan.propTypes = {
