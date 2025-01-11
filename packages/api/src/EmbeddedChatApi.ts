d1f074b6bb2e47dd156277181e4394e68ad705c8
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2025-01-11 21:57:59 +0530

    feat: add file type filtering dropdown to improve file navigation (#718)
    * feat: add file type filtering dropdown to improve file navigation
    
    * Update StaticSelect.styles.js
    
    * change layout of files search sidebar from col to row
    
    * fix: ensure selected value is persistent in placeholder

--- packages/api/src/EmbeddedChatApi.ts
+++ packages/api/src/EmbeddedChatApi.ts
@@ -732,21 +732,22 @@ export default class EmbeddedChatApi {
     }
   }
 
-  async getAllFiles(isChannelPrivate = false) {
+  async getAllFiles(isChannelPrivate = false, typeGroup: string) {
     const roomType = isChannelPrivate ? "groups" : "channels";
     try {
       const { userId, authToken } = (await this.auth.getCurrentUser()) || {};
-      const response = await fetch(
-        `${this.host}/api/v1/${roomType}.files?roomId=${this.rid}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "X-Auth-Token": authToken,
-            "X-User-Id": userId,
-          },
-          method: "GET",
-        }
-      );
+      const url =
+        typeGroup === ""
+          ? `${this.host}/api/v1/${roomType}.files?roomId=${this.rid}`
+          : `${this.host}/api/v1/${roomType}.files?roomId=${this.rid}&typeGroup=${typeGroup}`;
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Auth-Token": authToken,
+          "X-User-Id": userId,
+        },
+        method: "GET",
+      });
       return await response.json();
     } catch (err) {
       console.error(err);
