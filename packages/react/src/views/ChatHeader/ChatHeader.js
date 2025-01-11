727800ae4b70457d7fee2fe54ba32dd9071901c4
Piyush <157290995+thepiyush-303@users.noreply.github.com>
2025-01-11 17:07:43 +0530

    Feat: Add icon showing private channel (#867)
    * add lock icon
    
    * add icon
    
    * add lock file
    
    * fix size of headerIcon

--- packages/react/src/views/ChatHeader/ChatHeader.js
+++ packages/react/src/views/ChatHeader/ChatHeader.js
@@ -377,10 +377,14 @@ const ChatHeader = ({
                       onClick={() => setExclusiveState(setShowChannelinfo)}
                     >
                       <Icon
-                        name="hash"
+                        name={isChannelPrivate ? 'hash_lock' : 'hash'}
                         size={fullScreen ? '1.25rem' : '1rem'}
                       />
-                      <div>
+                      <div
+                        css={css`
+                          font-size: ${fullScreen ? '1.3rem' : '1.25rem'};
+                        `}
+                      >
                         {channelInfo.name || channelName || 'channelName'}
                       </div>
                     </Box>
