1f4c7f3fb98bab073ed594452362839d0c0ac394
KrishnaShuk <shuklakrishna.kris@gmail.com>
2025-02-13 20:13:06 +0530

    Added KaTeX icon/button to chat toolbar for documentation access

---
+++ packages/ui-elements/src/components/Icon/icons/KaTeX.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const KaTeX = (props) => (
+  <svg
+    width="21"
+    height="21"
+    viewBox="0 0 21 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4 4L8 10.5L4 17H6L9.25 11.5L6 6H4Z"
+      fill="currentColor"
+    />
+    <path
+      d="M17 4H10V6H12.5V15H10V17H17V15H14.5V6H17V4Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default KaTeX;
