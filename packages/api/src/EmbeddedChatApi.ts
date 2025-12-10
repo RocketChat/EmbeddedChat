a9ee3217d48666cde9085e22b2be98de93166a58
pratheek555 <pratheekravikumar55@gmail.com>
2025-12-10 18:12:48 +0000

    format checks

--- packages/api/src/EmbeddedChatApi.ts
+++ packages/api/src/EmbeddedChatApi.ts
@@ -665,14 +665,17 @@ export default class EmbeddedChatApi {
   async getUserRoles() {
     try {
       const { userId, authToken } = (await this.auth.getCurrentUser()) || {};
-      const response = await fetch(`${this.host}/api/v1/roles.getUsersInPublicRoles`, {
-        headers: {
-          "Content-Type": "application/json",
-          "X-Auth-Token": authToken,
-          "X-User-Id": userId,
-        },
-        method: "GET",
-      });
+      const response = await fetch(
+        `${this.host}/api/v1/roles.getUsersInPublicRoles`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Token": authToken,
+            "X-User-Id": userId,
+          },
+          method: "GET",
+        }
+      );
 
       const result = await response.json();
 
