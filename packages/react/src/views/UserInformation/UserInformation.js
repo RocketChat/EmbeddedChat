c5b6e7a132515a9ea38865363d0db793f94c0c5a
dodaa08 <dodaa08@users.noreply.github.com>
2026-01-15 15:29:28 +0530

    refactor: use @rocket.chat/message-parser instead of custom URL regex

--- packages/react/src/views/UserInformation/UserInformation.js
+++ packages/react/src/views/UserInformation/UserInformation.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { css } from '@emotion/react';
+import { parse } from '@rocket.chat/message-parser';
 import {
   Box,
   Sidebar,
@@ -18,7 +19,7 @@ import formatTimestampGetDate from '../../lib/formatTimestampGetDate';
 import UserInfoField from './UserInfoField';
 import getUserInformationStyles from './UserInformation.styles';
 import useSetExclusiveState from '../../hooks/useSetExclusiveState';
-import { parseUrls } from './parseUrls';
+import { Markdown } from '../Markdown';
 
 const UserInformation = () => {
   const { variantOverrides } = useComponentOverrides('UserInformation');
@@ -177,7 +178,12 @@ const UserInformation = () => {
             {currentUserInfo?.bio && (
               <UserInfoField
                 label="Bio"
-                value={parseUrls(currentUserInfo?.bio, theme, mode)}
+                value={
+                  <Markdown
+                    body={currentUserInfo.bio}
+                    md={parse(currentUserInfo.bio)}
+                  />
+                }
                 isAdmin={isAllowedToViewFullInfo}
                 authenticatedUserId={authenticatedUserId}
                 currentUserInfo={currentUserInfo}
