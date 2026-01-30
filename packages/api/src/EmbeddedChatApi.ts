2a8dee606f0ad4a391ab18b8363384bf1ab95fe0
Deepak Bhagat <deepak988088@gmail.com>
2026-01-30 06:45:19 +0530

    style: fix formatting in EmbeddedChatApi.ts

--- packages/api/src/EmbeddedChatApi.ts
+++ packages/api/src/EmbeddedChatApi.ts
@@ -71,21 +71,21 @@ export default class EmbeddedChatApi {
 
     const payload = acsCode
       ? JSON.stringify({
-        serviceName: "google",
-        accessToken: tokens.access_token,
-        idToken: tokens.id_token,
-        expiresIn: 3600,
-        totp: {
-          code: acsPayload,
-        },
-      })
+          serviceName: "google",
+          accessToken: tokens.access_token,
+          idToken: tokens.id_token,
+          expiresIn: 3600,
+          totp: {
+            code: acsPayload,
+          },
+        })
       : JSON.stringify({
-        serviceName: "google",
-        accessToken: tokens.access_token,
-        idToken: tokens.id_token,
-        expiresIn: 3600,
-        scope: "profile",
-      });
+          serviceName: "google",
+          accessToken: tokens.access_token,
+          idToken: tokens.id_token,
+          expiresIn: 3600,
+          scope: "profile",
+        });
 
     try {
       const req = await fetch(`${this.host}/api/v1/login`, {
@@ -551,9 +551,9 @@ export default class EmbeddedChatApi {
       query?: object | undefined;
       field?: object | undefined;
     } = {
-        query: undefined,
-        field: undefined,
-      },
+      query: undefined,
+      field: undefined,
+    },
     isChannelPrivate = false
   ) {
     const roomType = isChannelPrivate ? "groups" : "channels";
@@ -590,10 +590,10 @@ export default class EmbeddedChatApi {
       field?: object | undefined;
       offset?: number;
     } = {
-        query: undefined,
-        field: undefined,
-        offset: 50,
-      },
+      query: undefined,
+      field: undefined,
+      offset: 50,
+    },
     isChannelPrivate = false
   ) {
     const roomType = isChannelPrivate ? "groups" : "channels";
@@ -741,13 +741,13 @@ export default class EmbeddedChatApi {
     const messageObj =
       typeof message === "string"
         ? {
-          rid: this.rid,
-          msg: message,
-        }
+            rid: this.rid,
+            msg: message,
+          }
         : {
-          ...message,
-          rid: this.rid,
-        };
+            ...message,
+            rid: this.rid,
+          };
     if (threadId) {
       messageObj.tmid = threadId;
     }
