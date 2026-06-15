40a4e54e9b5f4a0eda20280d0bb8779b960f2223
Aryan-Verma-999 <anon999.420@gmail.com>
2026-06-15 23:36:45 +0530

    feat: transparent native matrix federation via joinRoom hook in EmbeddedChatApi

--- packages/react/src/views/EmbeddedChat.js
+++ packages/react/src/views/EmbeddedChat.js
@@ -18,7 +18,7 @@ import {
 import { ChatLayout } from './ChatLayout';
 import { ChatHeader } from './ChatHeader';
 import { RCInstanceProvider } from '../context/RCInstance';
-import { useUserStore, useLoginStore, useMessageStore } from '../store';
+import { useUserStore, useLoginStore, useMessageStore, useChannelStore } from '../store';
 import DefaultTheme from '../theme/DefaultTheme';
 import { getTokenStorage } from '../lib/auth';
 import { styles } from './EmbeddedChat.styles';
@@ -172,7 +172,8 @@ const EmbeddedChat = (props) => {
   useEffect(() => {
     const handleAuthChange = (user) => {
       if (user) {
-        RCInstance.connect()
+        const { isChannelPrivate } = useChannelStore.getState();
+        RCInstance.connect(isChannelPrivate)
           .then(() => {
             console.log(`Connected to RocketChat ${RCInstance.host}`);
             const me = user.me || user.data?.me;
