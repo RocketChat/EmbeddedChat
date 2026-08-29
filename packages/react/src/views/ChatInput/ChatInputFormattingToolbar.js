38c5ab5e639cc452d2c775fa46daa64d3291bfc8
iRahul360 <rahulburman669@gmail.com>
2026-08-29 09:57:56 +0530

    chore: format code

--- packages/react/src/views/ChatInput/ChatInputFormattingToolbar.js
+++ packages/react/src/views/ChatInput/ChatInputFormattingToolbar.js
@@ -370,7 +370,6 @@ const ChatInputFormattingToolbar = ({
         <EmojiPicker
           key="emoji-picker"
           handleEmojiClick={(emoji) => {
-          
             handleEmojiClick(emoji);
           }}
           onClose={() => setEmojiOpen(false)}
