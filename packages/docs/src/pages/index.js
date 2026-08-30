bad034067351dd4d2ce30c7d93cf1e18ab74e5a3
ishan kumar <289112142+ishan-one8@users.noreply.github.com>
2026-08-30 15:11:43 +0530

    fix(docs): add alt text to homepage logo image

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <img src={logo} />
+        <img src={logo} alt={`${siteConfig.title} logo`} />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
